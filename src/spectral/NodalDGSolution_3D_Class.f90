d45de200b1aab61e2892ac21f1913efaf0d11636
Joe <Joe>
2017-12-15 13:23:35 -0700

    Removed Doxygen style comments from NodalDGSolution_3D_Class.f90

--- src/spectral/NodalDGSolution_3D_Class.f90
+++ src/spectral/NodalDGSolution_3D_Class.f90
@@ -12,64 +12,60 @@ USE NodalDG_Class
 
 IMPLICIT NONE
 
-!> \addtogroup NodalDGSolution_3D 
-!! @{
-!!  The NodalDGSolution_3D class provides attributes for storing a solution and the flux and source
-!!  terms associated with a conservation law. Wrapper routines are provided that call routines in the
-!!  \ref NodalDG_Class to interpolate the solution array to the element boundaries and calculate the
-!!  flux divergence in both the strong and weak form.
-!!  
-!!  When building a Spectral Element solver, this class can be used to cleanly calculate the tendency in a 
-!!  PDE.
-!!
-!! <H2> NodalDGSolution_3D </H2>
-!! <H3> Attributes </H3>
-!!    <table>
-!!       <tr> <th> N <td> INTEGER  <td> Polynomial degree of the spectral element method
-!!
-!!       <tr> <th> nEquations <td> INTEGER <td> Number of solution variables.
-!!
-!!       <tr> <th> nElements <td> INTEGER <td> Number of elements that make up the spatial domain.
-!!
-!!       <tr> <th> solution(0:N,0:N,0:N,1:nEquations,1:nElements) <td> REAL(prec) <td> 
-!!
-!!       <tr> <th> flux(1:3,0:N,0:N,0:N,1:nEquations,1:nElements) <td> REAL(prec) <td> 
-!!
-!!       <tr> <th> boundarySolution(0:N,0:N,1:6,1:nEquations)* <td> REAL(prec) <td>
-!!
-!!       <tr> <th> boundaryFlux(0:N,0:N,1:6,1:nEquations)* <td> REAL(prec) <td>
-!!
-!!       <tr> <th> fluxDivergence(0:N,0:N,0:N,1:nEquations,1:nElements) <td> REAL(prec) <td> 
-!!
-!!       <tr> <th> source(0:N,0:N,0:N,1:nEquations,1:nElements) <td> REAL(prec) <td> 
-!!
-!!       <tr> <th> tendency(0:N,0:N,0:N,1:nEquations) <td> REAL(prec) <td>  
-!!
-!!    </table>
-!!
-!!  *For the "boundary" arrays, the faces of a hexahedral element are numbered as SOUTH=1, 
-!!  EAST=2, NORTH=3, WEST=4, BOTTOM=5, TOP=6.
-!!
-!! <H3> Procedures </H3>
-!!    <table>
-!!       <tr> <th> nodalSolution % Build( N, nEquations, nElements ) <td>
-!!       <tr> <th> nodalSolution % Trash( ) <td>
-!!       <tr> <th> nodalSolution % Calculate_Solution_at_Boundaries( nodalDGStorage ) <td>
-!!       <tr> <th> nodalSolution % Calculate_Weak_Flux_Divergence( nodalDGStorage ) <td>
-!!       <tr> <th> nodalSolution % Calculate_Strong_Flux_Divergence( nodalDGStorage ) <td>
-!!    </table>
-!!
-!!  If CUDA is enabled, additional routines are provided for updating the host-side arrays (CPU)
-!!  and the device-side arrays (GPU)
-!!
-!!    <table>
-!!      <tr> <th> nodalSolution % UpdateDevice( ) <td>
-!!      <tr> <th> nodalSolution % UpdateHost( )   <td>
-!!    </table>
-!!
-!!  * nodalDGStorage is an instance of the \ref NodalDG class.
-!!
-!>@}
+! ================================================================================================ !
+!  The NodalDGSolution_3D addtogroup provides attributes for storing a solution and the flux and source
+!  terms associated with a conservation law. 
+!
+!  Wrapper routines are provided that call routines in the
+!  NodalDG_Class to interpolate the solution array to the element boundaries and calculate the
+!  flux divergence in both the strong and weak form.
+!  
+!  When building a Spectral Element solver, this addtogroup can be used to cleanly calculate the tendency in a 
+!  PDE.
+!
+!  Attributes
+!    
+!     INTEGER  ::  N  <td> Polynomial degree of the spectral element method
+!
+!     INTEGER  ::  nEquations <td> Number of solution variables.
+!
+!     INTEGER  ::  nElements <td> Number of elements that make up the spatial domain.
+!
+!     REAL(prec)  ::  solution(0:N,0:N,0:N,1:nEquations,1:nElements)
+!
+!     REAL(prec)  ::  flux(1:3,0:N,0:N,0:N,1:nEquations,1:nElements)
+!
+!     REAL(prec)  ::  boundarySolution(0:N,0:N,1:6,1:nEquations)*
+!
+!     REAL(prec)  ::  boundaryFlux(0:N,0:N,1:6,1:nEquations)*
+!
+!     REAL(prec)  ::  fluxDivergence(0:N,0:N,0:N,1:nEquations,1:nElements)
+!
+!     REAL(prec)  ::  source(0:N,0:N,0:N,1:nEquations,1:nElements)
+!
+!     REAL(prec)  ::  tendency(0:N,0:N,0:N,1:nEquations)
+!
+!
+!     >> *For the "boundary" arrays, the faces of a hexahedral element are numbered as SOUTH=1, 
+!         EAST=2, NORTH=3, WEST=4, BOTTOM=5, TOP=6.
+!
+!  Procedures
+!
+!      nodalSolution % Build( N, nEquations, nElements ) 
+!      nodalSolution % Trash( )
+!      nodalSolution % Calculate_Solution_at_Boundaries( nodalDGStorage )
+!      nodalSolution % Calculate_Weak_Flux_Divergence( nodalDGStorage )
+!      nodalSolution % Calculate_Strong_Flux_Divergence( nodalDGStorage ) 
+!
+!      >> nodalDGStorage is an instance of the NodalDG class.
+!
+!      >> If CUDA is enabled, additional routines are provided for updating the host-side arrays
+!         (CPU) and the device-side arrays (GPU)
+!
+!      nodalSolution % UpdateDevice( )
+!      nodalSolution % UpdateHost( )
+!
+! ================================================================================================ !
 
   TYPE NodalDGSolution_3D
 
@@ -116,34 +112,31 @@ IMPLICIT NONE
 CONTAINS
 
 ! ================================================================================================ !
-!> \addtogroup NodalDGSolution_3D
-!! @{ 
-!> \fn Build_NodalDGSolution_3D  
-!! Allocates space for the NodalDGSolution_3D attributes and initializes arrays to 0.0_prec.
-!! 
-!! <H2> Usage : </H2> 
-!! <B>TYPE</B>(NodalDGSolution_3D) :: nodalSolution <BR>
-!! <B>INTEGER</B>                  :: N, nEquations, nElements <BR>
-!!
-!!   <B>CALL</B> nodalSolution % Build( N, nEquations, nElements ) <BR>
-!!
-!! <H2> Input/Output : </H2> 
-!!
-!! <B> nodalSolution <B> (out) <BR>
-!!    NodalDGSolution structure. All array attributes are allocated and initialized to 0.0_prec
-!!    and the polynomial degree, number of equations, and the number of elements are set upon exit.
-!!
-!! <B>N<B> (in) <BR>
-!!    Polynomial degree of the spectral element approximation
-!!
-!! <B>nEquations<B> (in) <BR>
-!!    The number of equations/variables to be held underneath this data structure
-!!
-!! <B>nElements<B> (in) <BR>
-!!    The number of elements in the spectral element mesh.
-!!
-!!
-!>@}
+! Build_NodalDGSolution_3D  
+!   Allocates space for the NodalDGSolution_3D attributes and initializes arrays to 0.0_prec.
+! 
+!   Usage :
+! 
+!     TYPE(NodalDGSolution_3D) :: nodalSolution 
+!     INTEGER                  :: N, nEquations, nElements
+!
+!       CALL nodalSolution % Build( N, nEquations, nElements )
+!
+!   Input/Output :
+!
+!     nodalSolution (out)
+!       NodalDGSolution structure. All array attributes are allocated and initialized to 0.0_prec
+!       and the polynomial degree, number of equations, and the number of elements are set upon exit.
+!
+!     N (in)
+!       Polynomial degree of the spectral element approximation
+!
+!     nEquations (in)
+!       The number of equations/variables to be held underneath this data structure
+!
+!     nElements (in)
+!       The number of elements in the spectral element mesh.
+!
 ! ================================================================================================ ! 
 
   SUBROUTINE Build_NodalDGSolution_3D( myDGS, N, nEquations, nElements )
@@ -198,24 +191,21 @@ CONTAINS
   END SUBROUTINE Build_NodalDGSolution_3D
 
 ! ================================================================================================ !
-!> \addtogroup NodalDGSolution_3D
-!! @{ 
-!> \fn Trash_NodalDGSolution_3D
-!! Frees memory held by the attributes of the NodalDGSolution_3D data structure.
-!! 
-!! <H2> Usage : </H2> 
-!! <B>TYPE</B>(NodalDGSolution_3D) :: nodalSolution <BR>
-!!
-!!     <B>CALL</B> nodalSolution % Trash( ) <BR>
-!! 
-!! <H2> Input/Output : </H2>
-!!
-!!  <B> nodalSolution <B> (in/out) <BR>
-!!    On output, the memory held by the array attributes has been deallocated. If CUDA is enabled
-!!    all DEVICE memory used by this structure is deallocated.
-!!
-!!   
-!>@}
+! Trash_NodalDGSolution_3D
+!   Frees memory held by the attributes of the NodalDGSolution_3D data structure.
+! 
+!   Usage :
+! 
+!     TYPE(NodalDGSolution_3D) :: nodalSolution
+!
+!     CALL nodalSolution % Trash( )
+! 
+!   Input/Output :
+!
+!     nodalSolution (in/out) 
+!       On output, the memory held by the array attributes has been deallocated. If CUDA is enabled
+!       all DEVICE memory used by this structure is deallocated.
+!   
 ! ================================================================================================ ! 
 
   SUBROUTINE Trash_NodalDGSolution_3D( myDGS )
@@ -246,31 +236,28 @@ CONTAINS
   END SUBROUTINE Trash_NodalDGSolution_3D
 
 ! ================================================================================================ !
-!> \addtogroup NodalDGSolution_3D
-!! @{ 
-!> \fn Calculate_Solution_At_Boundaries
-!!   Passes the solution attribute to the routine \ref CalculateFunctionsAtBoundaries_3D to calculate
-!!   the boundarySolution attribute. This wrapper routine passes the device attributes when CUDA is 
-!!   enabled. 
-!! 
-!! <H2> Usage : </H2> 
-!! <B>TYPE</B>(NodalDGSolution_3D) :: nodalSolution <BR>
-!! <B>TYPE</B>(NodalDG)            :: dgStorage <BR>
-!!
-!!     <B>CALL</B> nodalSolution % Calculate_Solution_At_Boundaries( dgStorage ) <BR>
-!! 
-!! <H2> Input/Output : </H2>
-!!
-!!  <B> nodalSolution <B> (in/out) <BR>
-!!      Instance of the \ref NodalDGSolution_3D structure that has been constructed. On output, the 
-!!      boundarySolution attribute is updated by interpolating the solution attribute to element
-!!      faces.
-!!
-!!  <B> dgStorage <B> (in) <BR>
-!!      Instance of the \ref NodalDG structure.
-!!
-!!   
-!>@}
+! Calculate_Solution_At_Boundaries
+!   Passes the solution attribute to the routine \ref CalculateFunctionsAtBoundaries_3D to calculate
+!   the boundarySolution attribute. This wrapper routine passes the device attributes when CUDA is 
+!   enabled. 
+! 
+!  Usage : </H2> 
+!    TYPE</B>(NodalDGSolution_3D) :: nodalSolution 
+!    TYPE</B>(NodalDG)            :: dgStorage 
+!
+!      CALL</B> nodalSolution % Calculate_Solution_At_Boundaries( dgStorage ) 
+! 
+!  Input/Output : </H2>
+!
+!    nodalSolution  (in/out) 
+!      Instance of the \ref NodalDGSolution_3D structure that has been constructed. On output, the 
+!      boundarySolution attribute is updated by interpolating the solution attribute to element
+!      faces.
+!
+!    dgStorage  (in) 
+!      Instance of the \ref NodalDG structure.
+!
+!   
 ! ================================================================================================ ! 
 
   SUBROUTINE Calculate_Solution_At_Boundaries( myDGS, dgStorage )
@@ -296,32 +283,29 @@ CONTAINS
   END SUBROUTINE Calculate_Solution_At_Boundaries
 
 ! ================================================================================================ !
-!> \addtogroup NodalDGSolution_3D
-!! @{ 
-!> \fn Calculate_Weak_Flux_Divergence
-!!   Passes the flux and boundaryFlux attributes to the routine \ref DG_Divergence_3D to calculate
-!!   the fluxDivergence attribute. This wrapper routine passes the device attributes when CUDA is 
-!!   enabled. This routine calculates the flux using the weak formulation of the divergence operator
-!!   in computational coordinates
-!!
-!! <H2> Usage : </H2> 
-!! <B>TYPE</B>(NodalDGSolution_3D) :: nodalSolution <BR>
-!! <B>TYPE</B>(NodalDG)            :: dgStorage <BR>
-!!
-!!     <B>CALL</B> nodalSolution % Calculate_Weak_Flux_Divergence( dgStorage ) <BR>
-!! 
-!! <H2> Input/Output : </H2>
-!!
-!!  <B> nodalSolution <B> (in/out) <BR>
-!!      Instance of the \ref NodalDGSolution_3D structure that has been constructed. On output, the 
-!!      boundarySolution attribute is updated by interpolating the solution attribute to element
-!!      faces.
-!!
-!!  <B> dgStorage <B> (in) <BR>
-!!      Instance of the \ref NodalDG structure.
-!!
-!!   
-!>@}
+! Calculate_Weak_Flux_Divergence
+!   Passes the flux and boundaryFlux attributes to the routine \ref DG_Divergence_3D to calculate
+!   the fluxDivergence attribute. This wrapper routine passes the device attributes when CUDA is 
+!   enabled. This routine calculates the flux using the weak formulation of the divergence operator
+!   in computational coordinates
+!
+!   Usage : </H2> 
+!
+!     TYPE</B>(NodalDGSolution_3D) :: nodalSolution 
+!     TYPE</B>(NodalDG)            :: dgStorage 
+!
+!       CALL</B> nodalSolution % Calculate_Weak_Flux_Divergence( dgStorage ) 
+! 
+!   Input/Output : </H2>
+!
+!     nodalSolution  (in/out) 
+!       Instance of the \ref NodalDGSolution_3D structure that has been constructed. On output, the 
+!       boundarySolution attribute is updated by interpolating the solution attribute to element
+!       faces.
+!
+!     dgStorage  (in) 
+!       Instance of the \ref NodalDG structure.
+!
 ! ================================================================================================ ! 
 
   SUBROUTINE Calculate_Weak_Flux_Divergence( myDGS, dgStorage )
@@ -350,32 +334,29 @@ CONTAINS
   END SUBROUTINE Calculate_Weak_Flux_Divergence
 
 ! ================================================================================================ !
-!> \addtogroup NodalDGSolution_3D
-!! @{ 
-!> \fn Calculate_Strong_Flux_Divergence
-!!   Passes the flux attribute to the routine \ref CalculateDivergence to calculate
-!!   the fluxDivergence attribute. This wrapper routine passes the device attributes when CUDA is 
-!!   enabled. This routine calculates the flux using the strong formulation of the divergence operator
-!!   in computational coordinates
-!!
-!! <H2> Usage : </H2> 
-!! <B>TYPE</B>(NodalDGSolution_3D) :: nodalSolution <BR>
-!! <B>TYPE</B>(NodalDG)            :: dgStorage <BR>
-!!
-!!     <B>CALL</B> nodalSolution % Calculate_Strong_Flux_Divergence( dgStorage ) <BR>
-!! 
-!! <H2> Input/Output : </H2>
-!!
-!!  <B> nodalSolution <B> (in/out) <BR>
-!!      Instance of the \ref NodalDGSolution_3D structure that has been constructed. On output, the 
-!!      boundarySolution attribute is updated by interpolating the solution attribute to element
-!!      faces.
-!!
-!!  <B> dgStorage <B> (in) <BR>
-!!      Instance of the \ref NodalDG structure.
-!!
-!!   
-!>@}
+! Calculate_Strong_Flux_Divergence
+!   Passes the flux attribute to the routine \ref CalculateDivergence to calculate
+!   the fluxDivergence attribute. This wrapper routine passes the device attributes when CUDA is 
+!   enabled. This routine calculates the flux using the strong formulation of the divergence operator
+!   in computational coordinates
+!
+!   Usage : </H2> 
+!
+!     TYPE</B>(NodalDGSolution_3D) :: nodalSolution 
+!     TYPE</B>(NodalDG)            :: dgStorage 
+!
+!       CALL</B> nodalSolution % Calculate_Strong_Flux_Divergence( dgStorage ) 
+! 
+!   Input/Output : </H2>
+!
+!     nodalSolution  (in/out) 
+!       Instance of the \ref NodalDGSolution_3D structure that has been constructed. On output, the 
+!       boundarySolution attribute is updated by interpolating the solution attribute to element
+!       faces.
+!
+!     dgStorage  (in) 
+!       Instance of the \ref NodalDG structure.
+!
 ! ================================================================================================ ! 
 
   SUBROUTINE Calculate_Strong_Flux_Divergence( myDGS, dgStorage )
@@ -403,25 +384,24 @@ CONTAINS
 #ifdef HAVE_CUDA
 
 ! ================================================================================================ !
-!> \addtogroup NodalDGSolution_3D
-!! @{ 
-!> \fn UpdateDevice_NodalDGSolution_3D 
-!!     Copies the host-side (CPU memory) attributes to the device-side (GPU memory) attributes. This
-!!     routine is only available if CUDA is enabled. The memory copy is done on the default stream
-!!     and is blocking on the GPU.
-!!
-!! <H2> Usage : </H2> 
-!! <B>TYPE</B>(NodalDGSolution_3D) :: nodalSolution <BR>
-!!
-!!     <B>CALL</B> nodalSolution % UpdateDevice( ) <BR>
-!! 
-!! <H2> Input/Output : </H2>
-!!
-!!  <B> nodalSolution <B> (in/out) <BR>
-!!      Instance of the \ref NodalDGSolution_3D structure that has been constructed.  
-!!      On output, the device-side attributes are updated wth the host-side attributes
-!!
-!>@}
+! UpdateDevice_NodalDGSolution_3D 
+!
+!   Copies the host-side (CPU memory) attributes to the device-side (GPU memory) attributes. This
+!   routine is only available if CUDA is enabled. The memory copy is done on the default stream
+!   and is blocking on the GPU.
+!
+!   Usage : </H2> 
+!
+!     TYPE</B>(NodalDGSolution_3D) :: nodalSolution 
+!
+!       CALL</B> nodalSolution % UpdateDevice( ) 
+! 
+!   Input/Output : </H2>
+!
+!     nodalSolution  (in/out) 
+!       Instance of the \ref NodalDGSolution_3D structure that has been constructed.  
+!       On output, the device-side attributes are updated wth the host-side attributes
+!
 ! ================================================================================================ ! 
 
   SUBROUTINE UpdateDevice_NodalDGSolution_3D( myDGS )
@@ -439,25 +419,24 @@ CONTAINS
   END SUBROUTINE UpdateDevice_NodalDGSolution_3D
 
 ! ================================================================================================ !
-!> \addtogroup NodalDGSolution_3D
-!! @{ 
-!> \fn UpdateHost_NodalDGSolution_3D 
-!!     Copies the device-side (GPU memory) attributes to the host-side (CPU memory) attributes. This
-!!     routine is only available if CUDA is enabled. The memory copy is done on the default stream
-!!     and is blocking on the GPU.
-!!
-!! <H2> Usage : </H2> 
-!! <B>TYPE</B>(NodalDGSolution_3D) :: nodalSolution <BR>
-!!
-!!     <B>CALL</B> nodalSolution % UpdateDevice( ) <BR>
-!! 
-!! <H2> Input/Output : </H2>
-!!
-!!  <B> nodalSolution <B> (in/out) <BR>
-!!      Instance of the \ref NodalDGSolution_3D structure that has been constructed.  
-!!      On output, the host-side attributes are updated wth the device-side attributes
-!!
-!>@}
+! UpdateHost_NodalDGSolution_3D 
+!
+!   Copies the device-side (GPU memory) attributes to the host-side (CPU memory) attributes. This
+!   routine is only available if CUDA is enabled. The memory copy is done on the default stream
+!   and is blocking on the GPU.
+!
+!   Usage : </H2> 
+!
+!     TYPE</B>(NodalDGSolution_3D) :: nodalSolution 
+!
+!       CALL</B> nodalSolution % UpdateDevice( ) 
+! 
+!   Input/Output : </H2>
+!
+!     nodalSolution  (in/out) 
+!       Instance of the \ref NodalDGSolution_3D structure that has been constructed.  
+!       On output, the host-side attributes are updated wth the device-side attributes
+!
 ! ================================================================================================ ! 
 
   SUBROUTINE UpdateHost_NodalDGSolution_3D( myDGS )
