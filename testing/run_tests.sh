770cf1b322fd07b08ff01d1cd0edca12bca198b9
Joe <Joe>
2018-01-17 18:30:14 -0700

    Added spectralfilter_tests to the run_tests.sh script

--- testing/run_tests.sh
+++ testing/run_tests.sh
@@ -11,10 +11,13 @@ export DOUBLE_PRECISION=yes
 
 make -s clean
 make -s spectral_tests
+make -s spectralfilter_tests
 
 echo "spectral_tests : serial cpu double precision"
-
 ./spectral_tests > cpu_spectral_tests.double.txt 
+
+echo "spectralfilter_tests : serial cpu double precision"
+./spectralfilter_tests > cpu_spectralfilter_tests.double.txt
 
 # ---------------------------------------------------------------------- #
 
@@ -22,10 +25,13 @@ export DOUBLE_PRECISION=no
 
 make -s clean
 make -s spectral_tests
+make -s spectralfilter_tests
 
 echo "spectral_tests : serial cpu single precision"
-
 ./spectral_tests > cpu_spectral_tests.single.txt 
+
+echo "spectralfilter_tests : serial cpu single precision"
+./spectralfilter_tests > cpu_spectralfilter_tests.single.txt
 
 # ---------------------------------------------------------------------- #
 
@@ -34,10 +40,13 @@ export DOUBLE_PRECISION=yes
 
 make -s clean
 make -s spectral_tests
+make -s spectralfilter_tests
 
 echo "spectral_tests : gpu double precision"
-
 ./spectral_tests > gpu_spectral_tests.double.txt 
+
+echo "spectralfilter_tests : gpu double precision"
+./spectralfilter_tests > gpu_spectralfilter_tests.double.txt
 
 # ---------------------------------------------------------------------- #
 
@@ -46,15 +55,22 @@ export DOUBLE_PRECISION=no
 
 make -s clean
 make -s spectral_tests
+make -s spectralfilter_tests
 
 echo "spectral_tests : gpu single precision"
-
 ./spectral_tests > gpu_spectral_tests.single.txt 
+
+echo "spectralfilter_tests : gpu single precision"
+./spectralfilter_tests > gpu_spectralfilter_tests.single.txt
 
 # ---------------------------------------------------------------------- #
 
 diff -q cpu_spectral_tests.single.txt references/cpu_spectral_tests.single.reference
 diff -q gpu_spectral_tests.single.txt references/gpu_spectral_tests.single.reference
+diff -q cpu_spectralfilter_tests.single.txt references/cpu_spectralfilter_tests.single.reference
+diff -q gpu_spectralfilter_tests.single.txt references/gpu_spectralfilter_tests.single.reference
 
 diff -q cpu_spectral_tests.double.txt references/cpu_spectral_tests.double.reference
 diff -q gpu_spectral_tests.double.txt references/gpu_spectral_tests.double.reference
+diff -q cpu_spectralfilter_tests.double.txt references/cpu_spectralfilter_tests.double.reference
+diff -q gpu_spectralfilter_tests.double.txt references/gpu_spectralfilter_tests.double.reference
