5df9cbdc7a165f2a42cf03bf0b176490ec6c82d6
Joe <joeschoonover@fluidnumerics.com>
2018-01-30 18:32:58 -0700

    Finished adding Faces_Class. Compile checked with and without CUDA

--- src/geom/Faces_Class.f90
+++ src/geom/Faces_Class.f90
@@ -13,9 +13,8 @@ USE ConstantsDictionary
 
 IMPLICIT NONE
 
-
   TYPE Faces
-    INTEGER              :: nFaces
+    INTEGER              :: nFaces, N
     INTEGER, ALLOCATABLE :: faceID(:)            ! The face ID
     INTEGER, ALLOCATABLE :: boundaryID(:)        ! If the face is part of the mesh boundary, the face gets assigned a boundary face ID
     INTEGER, ALLOCATABLE :: nodeIDs(:,:)      ! Node IDs which start and terminate this face
@@ -25,22 +24,162 @@ IMPLICIT NONE
     INTEGER, ALLOCATABLE :: jStart(:), jInc(:)      ! Loop start and increment for the secondary element side (2nd computational direction)
     INTEGER, ALLOCATABLE :: swapDimensions(:)    ! A flag used to swap the computational directions
 !                                         between the primary and secondary elements
+    INTEGER, ALLOCATABLE :: iMap(:,:,:)
+    INTEGER, ALLOCATABLE :: jMap(:,:,:)
 
 #ifdef HAVE_CUDA
-    INTEGER, DEVICE, ALLOCATABLE :: nFaces_dev
+    INTEGER, DEVICE, ALLOCATABLE :: nFaces_dev, N_dev
     INTEGER, DEVICE, ALLOCATABLE :: faceID_dev(:)            ! The face ID
     INTEGER, DEVICE, ALLOCATABLE :: boundaryID_dev(:)        ! If the face is part of the mesh boundary, the face gets assigned a boundary face ID
     INTEGER, DEVICE, ALLOCATABLE :: nodeIDs_dev(:,:)      ! Node IDs which start and terminate this face
     INTEGER, DEVICE, ALLOCATABLE :: elementIDs_dev(:,:)   ! Neighboring elements IDs across the face
     INTEGER, DEVICE, ALLOCATABLE :: elementSides_dev(:,:) ! Local side IDs for the neighboring elements
-    INTEGER, DEVICE, ALLOCATABLE :: iStart_dev(:), iInc_dev(:)      ! Loop start and increment for the secondary element side (1st computational direction)
-    INTEGER, DEVICE, ALLOCATABLE :: jStart_dev(:), jInc_dev(:)      ! Loop start and increment for the secondary element side (2nd computational direction)
-    INTEGER, DEVICE, ALLOCATABLE :: swapDimensions_dev(:)    ! A flag used to swap the computational directions
+    INTEGER, DEVICE, ALLOCATABLE :: iMap_dev(:,:,:)
+    INTEGER, DEVICE, ALLOCATABLE :: jMap_dev(:,:,:)
 
 #endif
 
      CONTAINS
 
+     PROCEDURE :: Build => Build_Faces
+     PROCEDURE :: Trash => Trash_Faces
+
+#ifdef HAVE_CUDA
+     PROCEDURE :: UpdateHost => UpdateHost_Faces
+     PROCEDURE :: UpdateDevice => UpdateDevice_Faces
+#endif
+
   END TYPE Faces
+
+
+CONTAINS
+
+SUBROUTINE Build_Faces( myFaces, nFaces, N )
+IMPLICIT NONE
+CLASS( Faces ), INTENT(out) :: myFaces
+INTEGER, INTENT(in)         :: nFaces, N
+
+
+myFaces % nFaces = nFaces
+myFaces % N      = N
+
+ALLOCATE( myFaces % faceID(1:nFaces), &
+          myFaces % boundaryID(1:nFaces), &
+          myFaces % nodeIDs(1:4,1:nFaces), &
+          myFaces % elementIDs(1:2,1:nFaces), &
+          myFaces % elementSides(1:2,1:nFaces), &
+          myFaces % iStart(1:nFaces), &
+          myFaces % iInc(1:nFaces), &
+          myFaces % jStart(1:nFaces), &
+          myFaces % jInc(1:nFaces), &
+          myFaces % swapDimensions(1:nFaces), &
+          myFaces % iMap(0:N,0:N,1:nFaces), & 
+          myFaces % jMap(0:N,0:N,1:nFaces) )
+          
+myFaces % faceID         = 0
+myFaces % boundaryID     = 0
+myFaces % nodeIDs        = 0
+myFaces % elementIDs     = 0
+myFaces % elementSides   = 0
+myFaces % iStart         = 0
+myFaces % iInc           = 0
+myFaces % jStart         = 0
+myFaces % jInc           = 0
+myFaces % swapDimensions = 0
+myFaces % iMap           = 0
+myFaces % jMap           = 0
+
+#ifdef HAVE_CUDA
+
+ALLOCATE( myFaces % nFaces_dev, myFaces % N_dev )
+
+myFaces % nFaces_dev = nFaces
+myFaces % N_dev      = N
+
+ALLOCATE( myFaces % faceID_dev(1:nFaces), &
+          myFaces % boundaryID_dev(1:nFaces), &
+          myFaces % nodeIDs_dev(1:4,1:nFaces), &
+          myFaces % elementIDs_dev(1:2,1:nFaces), &
+          myFaces % elementSides_dev(1:2,1:nFaces), &
+          myFaces % iMap_dev(0:N,0:N,1:nFaces), & 
+          myFaces % jMap_dev(0:N,0:N,1:nFaces) )
+          
+myFaces % faceID_dev       = 0
+myFaces % boundaryID_dev   = 0
+myFaces % nodeIDs_dev      = 0
+myFaces % elementIDs_dev   = 0
+myFaces % elementSides_dev = 0
+myFaces % iMap_dev         = 0
+myFaces % jMap_dev         = 0
+
+#endif
+
+END SUBROUTINE Build_Faces 
+!
+SUBROUTINE Trash_Faces( myFaces )
+IMPLICIT NONE
+CLASS( Faces ), INTENT(inout) :: myFaces
+
+
+
+DEALLOCATE( myFaces % faceID, &
+            myFaces % boundaryID, &
+            myFaces % nodeIDs, &
+            myFaces % elementIDs, &
+            myFaces % elementSides, &
+            myFaces % iStart, &
+            myFaces % iInc, &
+            myFaces % jStart, &
+            myFaces % jInc, &
+            myFaces % swapDimensions, &
+            myFaces % iMap, & 
+            myFaces % jMap )
+          
+#ifdef HAVE_CUDA
+
+DEALLOCATE( myFaces % nFaces_dev, myFaces % N_dev )
+DEALLOCATE( myFaces % faceID_dev, &
+            myFaces % boundaryID_dev, &
+            myFaces % nodeIDs_dev, &
+            myFaces % elementIDs_dev, &
+            myFaces % elementSides_dev, &
+            myFaces % iMap_dev, & 
+            myFaces % jMap_dev )
+          
+#endif
+
+END SUBROUTINE Trash_Faces 
+
+#ifdef HAVE_CUDA
+
+SUBROUTINE UpdateHost_Faces( myFaces )
+IMPLICIT NONE
+CLASS( Faces ), INTENT(inout) :: myFaces
+
+myFaces % faceID       = myFaces % faceID_dev
+myFaces % boundaryID   = myFaces % boundaryID_dev
+myFaces % nodeIDs      = myFaces % nodeIDs_dev
+myFaces % elementIDs   = myFaces % elementIDs_dev
+myFaces % elementSides = myFaces % elementSides_dev
+myFaces % iMap         = myFaces % iMap_dev
+myFaces % jMap         = myFaces % jMap_dev
+
+END SUBROUTINE UpdateHost_Faces
+
+SUBROUTINE UpdateDevice_Faces( myFaces )
+IMPLICIT NONE
+CLASS( Faces ), INTENT(inout) :: myFaces
+
+myFaces % faceID_dev       = myFaces % faceID
+myFaces % boundaryID_dev   = myFaces % boundaryID
+myFaces % nodeIDs_dev      = myFaces % nodeIDs
+myFaces % elementIDs_dev   = myFaces % elementIDs
+myFaces % elementSides_dev = myFaces % elementSides
+myFaces % iMap_dev         = myFaces % iMap
+myFaces % jMap_dev         = myFaces % jMap
+
+END SUBROUTINE UpdateDevice_Faces
+
+#endif
 
 END MODULE Faces_Class
