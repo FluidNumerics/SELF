0ecb9dd844cafefbbdb6b714f1efd156d7ca2783
Joe <joeschoonover@fluidnumerics.com>
2018-02-15 09:01:41 -0700

    Cleaned up Fluid Driver

--- src/fluid/Fluid_Driver.f90
+++ src/fluid/Fluid_Driver.f90
@@ -1,6 +1,6 @@
 ! Fluid_Driver.f90
 !
-! Copyright 2017 Joseph Schoonover <joe@fluidnumerics.consulting>, Fluid Numerics LLC
+! Copyright 2018 Joseph Schoonover <joe@fluidnumerics.consulting>, Fluid Numerics LLC
 ! All rights reserved.
 !
 ! //////////////////////////////////////////////////////////////////////////////////////////////// !
@@ -8,20 +8,14 @@
 
 PROGRAM Fluid_Driver
 
-! src/COMMON/
   USE ModelPrecision
 #ifdef TIMING
   USE Timing
 #endif
-! src/highend/euler/mpi-cuda/
-  USE FluidParams_CLASS
-  USE Fluid_CLASS
+  USE FluidParams_Class
+  USE Fluid_Class
 
   IMPLICIT NONE
-
-#ifdef INSITU_VIZ
-  INCLUDE "visitfortransimV2interface.inc"
-#endif
 
 ! <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><> !
 
@@ -33,14 +27,8 @@ PROGRAM Fluid_Driver
   TYPE( MultiTimers ) :: timers
 #endif
 
-#ifdef DIAGNOSTICS
   INTEGER             :: diagUnits(1:nDiagnostics)
-#endif
 
-#ifdef INSITU_VIZ
-  INTEGER             :: simulationCycle, simulationTime, runFlag
-  COMMON/SIMSTATE/ runFlag, simulationCycle, simulationTime
-#endif
 
 ! <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><> !
 
@@ -48,9 +36,6 @@ PROGRAM Fluid_Driver
   CALL Setup( )
 
   IF( setupSuccess )THEN
-#ifdef INSITU_VIZ
-    CALL SetupLibSim( )
-#endif
 
     CALL MainLoop( )
 
@@ -67,29 +52,15 @@ CONTAINS
   SUBROUTINE Setup( )
     IMPLICIT NONE
 
-    myeu % myRank = 0
-    myeu % nProc  = 1
-#ifdef HAVE_MPI
-    ! MPI Initialization
-    CALL MPI_INIT( mpiErr )
-    CALL MPI_COMM_RANK( MPI_COMM_WORLD, myeu % myRank, mpiErr )
-    CALL MPI_COMM_SIZE( MPI_COMM_WORLD, myeu % nProc, mpiErr )
-    PRINT*, 'Fluid_Driver : Greetings from Process ', myeu % myRank, ' of ', myeu % nProc
-#endif
     CALL myeu % Build( setupSuccess )
 
     IF( .NOT. setupSuccess )THEN
-#ifdef HAVE_MPI
-      CALL MPI_FINALIZE( mpiErr )
-#endif
       RETURN
     ENDIF
 
-#ifdef DIAGNOSTICS
     CALL myeu % OpenDiagnosticsFiles( diagUnits )
     CALL myeu % Diagnostics( )
     CALL myeu % WriteDiagnostics( diagUnits )
-#endif
 
 
 #ifdef TIMING
@@ -106,25 +77,6 @@ CONTAINS
 ! ------------------------------------------------------------------------------ !
 ! <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><> !
 ! ------------------------------------------------------------------------------ !
-#ifdef INSITU_VIZ
-  SUBROUTINE SetupLibSim( )
-    IMPLICIT NONE
-    INCLUDE "visitfortransimV2interface.inc"
-    INTEGER :: vizErr
-
-    vizErr = visitSetupEnv( )
-    vizErr = visitInitializeSim( "SELF-Fluids",11, &
-      "A simulation of compressible fluids",35, &
-      "/home/joe/SELF-Fluids/bin/", 26, &
-      VISIT_F77NULLSTRING, VISIT_F77NULLSTRINGLEN, &
-      VISIT_F77NULLSTRING, VISIT_F77NULLSTRINGLEN, &
-      VISIT_F77NULLSTRING, VISIT_F77NULLSTRINGLEN )
-
-  END SUBROUTINE SetupLibSim
-#endif
-! ------------------------------------------------------------------------------ !
-! <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><> !
-! ------------------------------------------------------------------------------ !
 
   SUBROUTINE Cleanup( )
     IMPLICIT NONE
@@ -132,19 +84,13 @@ CONTAINS
     CALL myeu % Trash( )
 
 
-#ifdef DIAGNOSTICS
     CALL myeu % CloseDiagnosticsFiles( diagUnits )
-#endif
 
 #ifdef TIMING
     IF( myeu % myRank == 0 )THEN
       CALL timers % Write_MultiTimers( )
       CALL timers % Trash( )
     ENDIF
-#endif
-
-#ifdef HAVE_MPI
-    CALL MPI_FINALIZE( mpiErr )
 #endif
 
   END SUBROUTINE Cleanup
@@ -156,116 +102,6 @@ CONTAINS
   SUBROUTINE MainLoop( )
     IMPLICIT NONE
     INTEGER    :: iT
-#ifdef INSITU_VIZ
-    INCLUDE "visitfortransimV2interface.inc"
-    INTEGER    :: visitState, visitResult, blocking, iterate
-
-    REAL(prec) :: x(0:myeu % params % nPlot, &
-      0:myeu % params % nPlot, &
-      0:myeu % params % nPlot, &
-      1:myeu % mesh % nElems )
-    REAL(prec) :: y(0:myeu % params % nPlot, &
-      0:myeu % params % nPlot, &
-      0:myeu % params % nPlot, &
-      1:myeu % mesh % nElems )
-    REAL(prec) :: z(0:myeu % params % nPlot, &
-      0:myeu % params % nPlot, &
-      0:myeu % params % nPlot, &
-      1:myeu % mesh % nElems )
-    REAL(prec) :: u(0:myeu % params % nPlot, &
-      0:myeu % params % nPlot, &
-      0:myeu % params % nPlot, &
-      1:myeu % mesh % nElems )
-    REAL(prec) :: v(0:myeu % params % nPlot, &
-      0:myeu % params % nPlot, &
-      0:myeu % params % nPlot, &
-      1:myeu % mesh % nElems )
-    REAL(prec) :: w(0:myeu % params % nPlot, &
-      0:myeu % params % nPlot, &
-      0:myeu % params % nPlot, &
-      1:myeu % mesh % nElems )
-    REAL(prec) :: density(0:myeu % params % nPlot, &
-      0:myeu % params % nPlot, &
-      0:myeu % params % nPlot, &
-      1:myeu % mesh % nElems )
-    REAL(prec) :: potentialTemp(0:myeu % params % nPlot, &
-      0:myeu % params % nPlot, &
-      0:myeu % params % nPlot, &
-      1:myeu % mesh % nElems )
-    REAL(prec) :: pressure(0:myeu % params % nPlot, &
-      0:myeu % params % nPlot, &
-      0:myeu % params % nPlot, &
-      1:myeu % mesh % nElems )
-
-
-    CALL myeu % ObtainPlottingMesh( x, y, z )
-
-    runFlag = 1
-    simulationCycle = 0
-    simulationTime  = myeu % simulationTime
-    !$OMP PARALLEL
-    DO ! Loop indefinitely; REALly breaks when visitState < 0
-
-      IF( runFlag == 1 )THEN
-        blocking = 0
-      ELSE
-        blocking = 1
-      ENDIF
-
-      visitState = visitDetectInput( blocking, -1 )
-
-      IF( visitState == 0 )THEN
-
-        CALL myeu % ForwardStepRK3( 1 ) ! Forward Step
-        simulationCycle = simulationCycle + 1
-        simulationTime = myeu % simulationTime
-#ifdef HAVE_CUDA
-        myeu % state % solution = myeu % state % solution_dev ! Update the host from the GPU
-#endif
-        CALL myeu % FluidStateAtPlottingPoints( u, v, w, density, potentialTemp, pressure )
-
-      ELSEIF( visitState == 1 )THEN
-
-        runFlag = 0
-        visitResult = visitAttemptConnection( )
-        IF( visitResult == 1 )THEN
-          PRINT*, 'VisIt connected!'
-        ELSE
-          PRINT*, 'VisIt did not connect!'
-        ENDIF
-
-      ELSEIF( visitState == 2 )THEN
-
-        runFlag = 0
-        IF( visitProcessEngineCommand() == 0 )THEN
-          visitResult = visitDisconnect( )
-          runFlag = 1
-        ENDIF
-
-      ELSEIF( visitState < 0 )THEN
-
-        EXIT
-
-      ENDIF
-
-      IF( MOD( simulationCycle, dFreq ) == 0 )THEN
-        !$OMP MASTER
-        CALL myeu % WritePickup(  )
-#ifdef DIAGNOSTICS
-        CALL myeu % Diagnostics( )
-        CALL myeu % WriteDiagnosticsFiles( diagUnits )
-#endif
-        !$OMP END MASTER
-      ENDIF
-
-    ENDDO
-    !$OMP END PARALLEL
-
-
-! ------------------------------------------------------------------------------ !
-! <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><> !
-! ------------------------------------------------------------------------------ !
-#else
 ! ------------------------------------------------------------------------------ !
 ! <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><> !
 ! ------------------------------------------------------------------------------ !
@@ -302,225 +138,15 @@ CONTAINS
       CALL myeu % WriteTecplot( )
       !$OMP END MASTER
 
-#ifdef DIAGNOSTICS
       CALL myeu % Diagnostics( )
       CALL myeu % WriteDiagnostics( diagUnits )
-#endif
 
     ENDDO
     !$OMP END PARALLEL
 
-#endif
 
   END SUBROUTINE MainLoop
 
 
 END PROGRAM Fluid_Driver
 
-! ------------------------------------------------------------------------------ !
-! <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><> !
-! ------------------------------------------------------------------------------ !
-#ifdef INSITU_VIZ
-SUBROUTINE VisitCommandCallBack (cmd, lcmd, intDATA,   &
-  floatDATA, stringDATA,&
-  lstringDATA )
-  IMPLICIT NONE
-  INCLUDE "visitfortransimV2interface.inc"
-  CHARACTER(8) :: cmd, stringDATA
-  INTEGER      :: lcmd, lstringDATA, intDATA
-  REAL         :: floatDATA
-  INTEGER      :: runflag, simcycle
-  REAL         :: simtime
-
-  IF( visitstrcmp(cmd, lcmd, "halt", 4) == 0)THEN
-    runflag = 0
-    ! ELSEIF(visitstrcmp(cmd, lcmd, "step", 4).eq.0) THEN
-    !     CALL SimulateOneTimeStep( )
-  ELSEIF(visitstrcmp(cmd, lcmd, "run", 3).eq.0) THEN
-    runflag = 1
-  ELSEIF(visitstrcmp(cmd, lcmd, "testcommand", 11).eq.0) THEN
-    write (6,*) 'Received testcommand'
-  endIF
-
-END SUBROUTINE VisitCommandCallBack
-!
-INTEGER FUNCTION VisitBroadCastIntFUNCTION(value, sender)
-  IMPLICIT NONE
-  INTEGER :: value, sender
-
-  VisitBroadCastIntFUNCTION = 0
-
-END FUNCTION VisitBroadCastIntFUNCTION
-!
-INTEGER FUNCTION VisitBroadCastStringFUNCTION(str, lstr, sender)
-  IMPLICIT NONE
-  CHARACTER(8) :: str
-  INTEGER      :: lstr, sender
-
-  VisitBroadCastStringFUNCTION = 0
-
-END FUNCTION VisitBroadCastStringFUNCTION
-!
-SUBROUTINE VisitSlaveProcessCallBack( )
-  IMPLICIT NONE
-END SUBROUTINE VisitSlaveProcessCallBack
-!
-INTEGER FUNCTION VisitGetMetaDATA( )
-  IMPLICIT NONE
-  !INTEGER :: handle
-  INCLUDE "visitfortransimV2interface.inc"
-  INTEGER :: runFlag, simulationCycle, simulationTime
-  COMMON /SIMSTATE/ runFlag, simulationCycle, simulationTime
-  INTEGER :: err, md, m1, cmd, vmd
-
-
-  IF( VisitMDSimAlloc( md ) == VISIT_OKAY )THEN
-    err = VisitMDSimSetCycleTime( md, simulationCycle, simulationTime )
-    IF( runFlag == 1 )THEN
-      err = VisitMDSimSetMode( md, VISIT_SIMMODE_RUNNING )
-    ELSE
-      err = VisitMDSimSetMode( md, VISIT_SIMMODE_STOPPED )
-    ENDIF
-  ENDIF
-
-  VisitGetMetaDATA = md
-
-  ! Set the Mesh Meta-DATA
-  IF( VisitMDMeshAlloc(m1) == VISIT_OKAY )THEN
-    err = VisitMDMeshSetName( m1, "Mesh", 4 )
-    err = VisitMDMeshSetMeshTYPE( m1, VISIT_MESHTYPE_UNSTRUCTURED )
-    err = VisitMDMeshSetTopoLOGICALDim( m1, 3 )
-    err = VisitMDMeshSetSpatialDim( m1, 3 )
-    err = VisitMDMeshSetXUnits( m1, "m", 1 )
-    err = VisitMDMeshSetYUnits( m1, "m", 1 )
-    err = VisitMDMeshSetZUnits( m1, "m", 1 )
-    err = VisitMDMeshSetXLabel( m1, "x", 1 )
-    err = VisitMDMeshSetYLabel( m1, "y", 1 )
-    err = VisitMDMeshSetZLabel( m1, "z", 1 )
-
-    err = VisitMDSimAddMesh( md, m1 )
-  ENDIF
-
-  ! Add the density variable
-  IF( VisitMDvarAlloc( vmd ) == VISIT_OKAY )THEN
-    err = VisitMDvarSetName( vmd, "density", 7 )
-    err = VisitMDvarSetMeshName( vmd, "Mesh", 4 )
-    err = VisitMDvarSetTYPE( vmd, VISIT_VARTYPE_SCALAR )
-    err = VisitMDvarSetCentering( vmd, VISIT_VARCENTERING_NODE)
-    err = VisitMDSimAddVariable( md, vmd )
-  ENDIF
-
-  ! Add meta-DATA for simulation control commands
-  IF( VisitMDcmdAlloc( cmd ) == VISIT_OKAY )THEN
-    err = VisitMDcmdSetName( cmd, "halt", 4 )
-    err = VisitMDSimAddGenericCommand( md, cmd )
-  ENDIF
-  IF( VisitMDcmdAlloc( cmd ) == VISIT_OKAY )THEN
-    err = VisitMDcmdSetName( cmd, "run", 3 )
-    err = VisitMDSimAddGenericCommand( md, cmd )
-  ENDIF
-  !  IF( VisitMDcmdAlloc( cmd ) == VISIT_OKAY )THEN
-  !     err = VisitMDcmdSetName( cmd, "step", 4 )
-  !     err = VisitMDSimAddGenericCommand( md, cmd )
-  !  ENDIF
-
-
-
-
-
-END FUNCTION VisitGetMetaDATA
-!
-INTEGER FUNCTION VisitGetMesh(handle, DOmain, name, lname)
-  IMPLICIT NONE
-  CHARACTER(8) :: name
-  INTEGER      :: handle, DOmain, lname
-  INCLUDE "visitfortransimV2interface.inc"
-
-  VisitGetMesh = VISIT_ERROR
-
-END FUNCTION VisitGetMesh
-!
-INTEGER FUNCTION VisitGetMaterial(handle, DOmain, name, lname)
-  IMPLICIT NONE
-  CHARACTER(8) :: name
-  INTEGER      :: handle, DOmain, lname
-  INCLUDE "visitfortransimV2interface.inc"
-
-  VisitGetMaterial = VISIT_ERROR
-
-END FUNCTION VisitGetMaterial
-!
-INTEGER FUNCTION VisitGetDOmainNesting(handle, DOmain, name, lname)
-  IMPLICIT NONE
-  CHARACTER(8) :: name
-  INTEGER      :: handle, DOmain, lname
-  INCLUDE "visitfortransimV2interface.inc"
-
-  VisitGetDOmainNesting = VISIT_ERROR
-
-END FUNCTION VisitGetDOmainNesting
-!
-INTEGER FUNCTION VisitGetDOmainBounds(handle, DOmain, name, lname)
-  IMPLICIT NONE
-  CHARACTER(8) :: name
-  INTEGER      :: handle, DOmain, lname
-  INCLUDE "visitfortransimV2interface.inc"
-
-  VisitGetDOmainBounds = VISIT_ERROR
-
-END FUNCTION VisitGetDOmainBounds
-!
-INTEGER FUNCTION VisitGetDOmainList(handle, DOmain, name, lname)
-  IMPLICIT NONE
-  CHARACTER(8) :: name
-  INTEGER      :: handle, DOmain, lname
-  INCLUDE "visitfortransimV2interface.inc"
-
-  VisitGetDOmainList = VISIT_ERROR
-
-END FUNCTION VisitGetDOmainList
-!
-INTEGER FUNCTION VisitGetCurve(handle, DOmain, name, lname)
-  IMPLICIT NONE
-  CHARACTER(8) :: name
-  INTEGER      :: handle, DOmain, lname
-  INCLUDE "visitfortransimV2interface.inc"
-
-  VisitGetCurve = VISIT_ERROR
-
-END FUNCTION VisitGetCurve
-!
-INTEGER FUNCTION VisitGetMixedVariable(handle, DOmain, name, lname)
-  IMPLICIT NONE
-  CHARACTER(8) :: name
-  INTEGER      :: handle, DOmain, lname
-  INCLUDE "visitfortransimV2interface.inc"
-
-  VisitGetMixedVariable = VISIT_ERROR
-
-END FUNCTION VisitGetMixedVariable
-!
-INTEGER FUNCTION VisitGetVariable(handle, DOmain, name, lname)
-  IMPLICIT NONE
-  CHARACTER(8) :: name
-  INTEGER      :: handle, DOmain, lname
-  INCLUDE "visitfortransimV2interface.inc"
-
-  VisitGetVariable = VISIT_ERROR
-
-END FUNCTION VisitGetVariable
-!
-INTEGER FUNCTION VisitActivateTimeStep(handle, DOmain, name, lname)
-  IMPLICIT NONE
-  CHARACTER(8) :: name
-  INTEGER      ::  handle, DOmain, lname
-  INCLUDE "visitfortransimV2interface.inc"
-
-  VisitActivateTimeStep = VISIT_ERROR
-
-END FUNCTION VisitActivateTimeStep
-!
-
-#endif
-
-!END PROGRAM Fluid_Driver
