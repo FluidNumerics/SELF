7d007eef6d3e84fb2e4e75e0bcf5d740d1844005
Joe Schoonover <joe@fluidnumerics.com>
2020-05-17 18:20:22 -0600

    Add ScalarInterp3D with tests in Lagrange_Test
    * Need to add vector and tensor interp for 2D and 3D, then add
    the gradient, divergence, and curl routines for 2D and 3D.

--- src/self/Lagrange_HIP.cpp
+++ src/self/Lagrange_HIP.cpp
@@ -53,6 +53,39 @@ extern "C"
   } 
 }
 
+// ScalarGridInterp_3D //
+__global__ void ScalarGridInterp_3D_gpu(real *iMatrix, real *f, real *fInterp, int N, int M, int nVar){
+
+  size_t iVar = hipBlockIdx_x;
+  size_t iEl = hipBlockIdx_y;
+  size_t i = hipThreadIdx_x;
+  size_t j = hipThreadIdx_y;
+  size_t k = hipThreadIdx_z;
+
+  real fijk = 0.0;
+  for (int kk=0; kk<N+1; kk++) {
+    real fij = 0.0;
+    for (int jj=0; jj<N+1; jj++) {
+      real fi = 0.0;
+      for (int ii=0; ii<N+1; ii++) {
+        fi += f[SC_3D_INDEX(ii,jj,kk,iVar,iEl,N,nVar)]*iMatrix[ii+i*(N+1)];
+      }
+      fij += fi*iMatrix[jj+j*(N+1)];
+    }
+    fijk += fij*iMatrix[kk+k*(N+1)];
+  }
+  fInterp[SC_3D_INDEX(i,j,k,iVar,iEl,M,nVar)] = fijk;
+
+}
+
+extern "C"
+{
+  void ScalarGridInterp_3D_gpu_wrapper(real **iMatrix, real **f, real **fInterp, int N, int M, int nVar, int nEl)
+  {
+	  hipLaunchKernelGGL((ScalarGridInterp_3D_gpu), dim3(nVar,nEl,1), dim3(M+1,M+1,M+1), 0, 0, *iMatrix, *f, *fInterp, N, M, nVar);
+  } 
+}
+
 
 // GridDerivative_1D //
 __global__ void GridDerivative_1D_gpu(real *dMatrix, real *f, real *df, int N, int nVar){
