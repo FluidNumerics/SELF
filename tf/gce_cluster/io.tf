70fd0c44a345d8556d6d2d1ef7d01ca0cddb4b34
Joe Schoonover <joe@fluidnumerics.com>
2021-06-20 15:06:41 +0000

    Pull in gce_cluster template updates from hpc-apps-gcp

--- tf/gce_cluster/io.tf
+++ tf/gce_cluster/io.tf
@@ -7,7 +7,7 @@ variable "project" {
 variable "machine_type" {
   type = string
   description = "GCE instance types for your scheduler-less cluster. Learn more at https://cloud.google.com/compute/docs/machine-types."
-  default = "n1-standard-2"
+  default = "n1-standard-8"
 }
 
 variable "node_count" {
@@ -30,7 +30,7 @@ variable "tags" {
 variable "image" {
   type = string
   description = "VM image used to launch your HPC application"
-  default = "projects/hpc-apps/global/images/openmpi-singularity-latest"
+  default = "projects/hpc-apps/global/images/gromacs-gcp-latest"
 }
 
 variable "disk_size_gb" {
@@ -48,13 +48,13 @@ variable "disk_type" {
 variable "labels" {
   type = map
   description = "Resource labels to apply to each GCE instance in your cluster."
-  default = {"hpc-app"="self"}
+  default = {"hpc-app"="gromacs"}
 }
 
 variable "name_prefix" {
   type = string
   description = "The name to prefix all GCE instances in your cluster"
-  default = "self"
+  default = "gromacs"
 }
 
 variable "gpu_type" {
@@ -65,14 +65,26 @@ variable "gpu_type" {
 
 variable "gpu_count" {
   type = number
-  description = "Number of GPU accelerators to attach to your GCE instance."
+  description = "Number of GPU Accelerators to attach to your GCE instance."
   default = 0
 }
 
 variable "vpc_subnet" {
   type = string
   description = "VPC subnet (self-link) to use for your cluster."
-  default = "default"
+  default = ""
+}
+
+variable "subnet_cidr" {
+  type = string
+  description = "CIDR Range for the subnet (if vpc_subnet is not provided)."
+  default = "10.10.0.0/16"
+}
+
+variable "whitelist_ssh_ips" {
+  type = list(string)
+  description = "IP addresses that should be added to a whitelist for ssh access"
+  default = ["0.0.0.0/0"]
 }
 
 output "head_node_ip_addr" {
--- tf/gce_cluster/main.tf
+++ tf/gce_cluster/main.tf
@@ -3,11 +3,74 @@ provider "google" {
 }
 
 resource "google_service_account" "gce_service_account" {
-  account_id = "gromacs"
-  display_name = "GROMACS Service account"
+  account_id = "self-service"
+  display_name = "SELF Service account"
   project = var.project
 }
 
+locals {
+  region = trimsuffix(var.zone,substr(var.zone,-2,-2))
+}
+
+
+// **** Create the Shared VPC Network **** //
+resource "google_compute_network" "vpc_network" {
+  count = var.vpc_subnet == "" ? 1:0
+  name = "${var.name_prefix}-network"
+  project = var.project
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  count = var.vpc_subnet == "" ? 1:0
+  name = "${var.name_prefix}-subnet"
+  ip_cidr_range = var.subnet_cidr
+  region = local.region
+  network = google_compute_network.vpc_network[0].self_link
+  project = var.project
+}
+
+resource "google_compute_firewall" "default_ssh_firewall_rules" {
+  count = var.vpc_subnet == "" ? 1:0
+  name = "${var.name_prefix}-ssh"
+  network = google_compute_network.vpc_network[0].self_link
+  target_tags = [var.name_prefix]
+  source_ranges = var.whitelist_ssh_ips
+  project = var.project
+
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
+  }
+}
+
+resource "google_compute_firewall" "default_internal_firewall_rules" {
+  count = var.vpc_subnet == "" ? 1:0
+  name = "${var.name_prefix}-all-internal"
+  network = google_compute_network.vpc_network[0].self_link
+  source_tags = [var.name_prefix]
+  target_tags = [var.name_prefix]
+  project = var.project
+
+  allow {
+    protocol = "tcp"
+    ports = ["0-65535"]
+  }
+  allow {
+    protocol = "udp"
+    ports = ["0-65535"]
+  }
+  allow {
+    protocol = "icmp"
+    ports = []
+  }
+}
+
+
+locals {
+  subnet = var.vpc_subnet == "" ? google_compute_subnetwork.subnet[0].self_link:var.vpc_subnet
+  tags = var.vpc_subnet == "" ? [var.name_prefix]: var.tags
+}
 
 // ***************************************** //
 // Create the controller
@@ -17,7 +80,7 @@ resource "google_compute_instance" "gce_nodes" {
   project = var.project
   machine_type = var.machine_type
   zone = var.zone
-  tags = var.tags
+  tags = local.tags
   boot_disk {
     auto_delete = true
     initialize_params {
@@ -40,7 +103,7 @@ resource "google_compute_instance" "gce_nodes" {
         content {}
     }
 
-    subnetwork  = var.vpc_subnet
+    subnetwork  = local.subnet
   }
 
   dynamic "guest_accelerator" {
