40ed0dd4c6322276210bef3439feda3cc2f97aef
Joe Schoonover <joe@fluidnumerics.com>
2021-06-24 14:58:12 +0000

    Increase default disk size to 50GB
    * The singularity image from hpc-apps is set to 50GB; this enforces a
    mininum disk size we need to meet.

--- tf/gce/io.tf
+++ tf/gce/io.tf
@@ -36,7 +36,7 @@ variable "image" {
 variable "disk_size_gb" {
   type = number
   description = "Size of each GCE instance boot disk in your cluster."
-  default = 20
+  default = 50
 }
 
 variable "disk_type" {
