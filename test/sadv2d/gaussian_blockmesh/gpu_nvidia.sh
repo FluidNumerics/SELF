362c0b483c2292ead3c329f6466960429c4bc1d9
Joe Schoonover <joe@fluidnumerics.com>
2021-12-01 18:48:11 -0700

    Clean up test scripts and test dictionary

--- test/sadv2d/gaussian_blockmesh/gpu_nvidia.sh
+++ test/sadv2d/gaussian_blockmesh/gpu_nvidia.sh
@@ -3,12 +3,8 @@
 source /etc/profile.d/z10_spack_environment.sh
 spack load singularity
 
-mkdir -p ${WORKSPACE}/ci/test/codecov
+mkdir -p ${WORKSPACE}/codecov
 
-singularity run --nv --bind ${WORKSPACE}/ci/test/codecov:/build \
+singularity run --nv --bind ${WORKSPACE}/codecov:/build \
                             ${SINGULARITY_IMAGE} /opt/self/bin/sadv2d \
-			    -gpu \
-			    -dt 0.001 \
-			    -tn 1.0 \
-			    -t0 0.0 \
-			    -oi 0.5
+			    --gpu
--- test/sadv2d/gaussian_square_hohqmesh/gpu_nvidia.sh
+++ test/sadv2d/gaussian_square_hohqmesh/gpu_nvidia.sh
@@ -7,9 +7,5 @@ mkdir -p ${WORKSPACE}/codecov
 
 singularity run --nv --bind ${WORKSPACE}/codecov:/build \
                             ${SINGULARITY_IMAGE} /opt/self/bin/sadv2d \
-			    -gpu \
-			    -dt 0.001 \
-			    -tn 1.0 \
-			    -t0 0.0 \
-			    -oi 0.5
+			    --gpu \
                             --mesh "${WORKSPACE}/test/sadv2d/gaussian_square_hohqmesh/Square.mesh"
