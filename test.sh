b611de3864e350052a924ce9c9d4b40a33819079
Joe Schoonover <joe@fluidnumerics.com>
2022-02-16 02:48:38 +0000

    Remove redundant .gcno file moves

--- test.sh
+++ test.sh
@@ -4,39 +4,40 @@
 SELF_BUILD_DIR=/build
 SELF_INSTALL_DIR=/opt/self
 
-moveCovFiles(){
+moveGCNOFiles(){
 
   for file in $(ls $SELF_BUILD_DIR/src/*.gcno); do
     dest=$(echo $file | sed 's/\.f//g')
-    echo "$file -> $dest"
-    cp $file $dest
-  done
-
-  for file in $(ls $SELF_BUILD_DIR/src/*.gcda); do
-    dest=$(echo $file | sed 's/\.f//g')
-    echo "$file -> $dest"
     cp $file $dest
   done
 
   for file in $(ls $SELF_BUILD_DIR/examples/*.gcno); do
     dest=$(echo $file | sed 's/\.f90//g')
-    echo "$file -> $dest"
+    cp $file $dest
+  done
+
+}
+
+moveGCDAFiles(){
+
+  for file in $(ls $SELF_BUILD_DIR/src/*.gcda); do
+    dest=$(echo $file | sed 's/\.f//g')
     cp $file $dest
   done
 
   for file in $(ls $SELF_BUILD_DIR/examples/*.gcda); do
     dest=$(echo $file | sed 's/\.f90//g')
-    echo "$file -> $dest"
     cp $file $dest
   done
 
 }
 
-cleanupCovFiles(){
+cleanupGCDAFiles(){
   rm $SELF_BUILD_DIR/examples/*.gcda
   rm $SELF_BUILD_DIR/src/*.gcda
 }
 
+moveGCNOFiles
 tomerge=""
 for file in $(ls $SELF_INSTALL_DIR/examples/*); do
 
@@ -48,20 +49,13 @@ for file in $(ls $SELF_INSTALL_DIR/examples/*); do
     exit $rc
   fi
 
-  moveCovFiles
-
-#      for file in $(ls /build/src/*.f90); do
-#        gcov --function-summaries \
-#             --human-readable \
-#             --demangled-names \
-#             $file
-#      done
+  moveGCDAFiles
 
   covFile=$(echo $file | awk -F "/" '{print $NF}')
   gcovr -r /build --json -o "/build/$covFile.json"
   tomerge+="-a /build/$covFile.json "
 
-  cleanupCovFiles
+  cleanupGCDAFiles
 
 done
 
