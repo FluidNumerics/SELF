2bd007fc6cd56c4b5670852a054996984cc4d72c
Joe Schoonover <joe@fluidnumerics.com>
2023-12-23 18:44:29 +0000

    Add Noether installation script

---
+++ util/install_noether.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+GPU_TARGET=gfx90a
+WORKSPACE=$HOME/.local/workspace/
+BUILD_TYPE=coverage
+SRC_DIR=$(pwd)
+BUILD_DIR=${SRC_DIR}/build
+
+set -x
+module load gcc/13.2.0
+module load openmpi hdf5 feq-parse
+
+rm -rf ${BUILD_DIR}
+mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
+
+FC=gfortran \
+FFLAGS="-DDOUBLE_PRECISION" \
+cmake -DCMAKE_PREFIX_PATH=/opt/rocm \
+      -DCMAKE_HIP_ARCHITECTURES=${GPU_TARGET} \
+      -DCMAKE_INSTALL_PREFIX=${WORKSPACE}/opt/self \
+      -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
+      ${SRC_DIR}
+make VERBOSE=1
+make install
+
+
+# Initialize coverage
+mkdir -p ${WORKSPACE}/tmp/
+lcov --no-external \
+      --capture \
+      --initial \
+      --directory ${SRC_DIR} \
+      --output-file ${WORKSPACE}/tmp/lcov_base.info
+
+# Run ctests
+ctest --test-dir ${BUILD_DIR}/test
+
+# Compile coverage information
+lcov --no-external \
+    --capture \
+    --directory ${SRC_DIR} \
+    --output-file ${WORKSPACE}/tmp/lcov_test.info
+
+lcov --add-tracefile ${WORKSPACE}/tmp/lcov_base.info \
+     --add-tracefile ${WORKSPACE}/tmp/lcov_test.info \
+     --output-file ${SRC_DIR}/lcov.info
