8a0977dd3cdc7dc392eeff546c000ef04aeaeb06
Joe Schoonover <joe@fluidnumerics.com>
2024-01-29 14:42:52 +0000

    Add advection_diffusion_1d examples for each time integrator

--- src/SELF_advection_diffusion_1d.f90
+++ src/SELF_advection_diffusion_1d.f90
@@ -1,4 +1,4 @@
-module advection_diffusion_model_1d
+module self_advection_diffusion_1d
 
 use self_model
 use self_model1d
@@ -6,18 +6,17 @@ use self_mesh
 
 implicit none
 
-  type, extends(model1d) :: advdiff1d
+  type, extends(model1d) :: advection_diffusion_1d
     real(prec) :: nu ! diffusion coefficient
     real(prec) :: u  ! constant velocity
 
     contains
-    procedure :: pretendency => pretendency_advdiff1d
-    procedure :: setboundarycondition => setboundarycondition_advdiff1d
-!    procedure :: sourcemethod => sourcemethod_advdiff1d
-    procedure :: riemannsolver => riemannsolver_advdiff1d
-    procedure :: fluxmethod => fluxmethod_advdiff1d
+    procedure :: pretendency => pretendency_advection_diffusion_1d
+    procedure :: setboundarycondition => setboundarycondition_advection_diffusion_1d
+    procedure :: riemannsolver => riemannsolver_advection_diffusion_1d
+    procedure :: fluxmethod => fluxmethod_advection_diffusion_1d
 
-  end type advdiff1d
+  end type advection_diffusion_1d
 
 ! Remember, the order of operations for the tendency calculation is
 !
@@ -32,13 +31,13 @@ implicit none
 
 contains
 
-  subroutine pretendency_advdiff1d(this)
+  subroutine pretendency_advection_diffusion_1d(this)
   ! Here, we use the pre-tendency method to calculate the
   ! derivative of the solution using a bassi-rebay method
   ! We then do a boundary interpolation and side exchange
   ! on the gradient field
   implicit none
-  class(advdiff1d), intent(inout) :: this
+  class(advection_diffusion_1d), intent(inout) :: this
   ! local
   integer :: ivar
   integer :: N, nelem
@@ -77,16 +76,16 @@ contains
 
     call this % solutionGradient % BassiRebaySides(this % gpuaccel)
 
-  end subroutine pretendency_advdiff1d
+  end subroutine pretendency_advection_diffusion_1d
 
-  subroutine setboundarycondition_advdiff1d(this)
+  subroutine setboundarycondition_advection_diffusion_1d(this)
   ! Here, we set the boundary conditions for the 
   ! solution and the solution gradient at the left
   ! and right most boundaries.
   ! 
   ! Here, we use periodic boundary conditions
   implicit none
-  class(advdiff1d), intent(inout) :: this
+  class(advection_diffusion_1d), intent(inout) :: this
   ! local
   integer :: ivar
   integer :: N, nelem
@@ -113,11 +112,11 @@ contains
         this% solutionGradient % boundary % hostdata(ivar,2,nelem))
     enddo
 
-  end subroutine setboundarycondition_advdiff1d
+  end subroutine setboundarycondition_advection_diffusion_1d
 
-  subroutine fluxmethod_advdiff1d(this)
+  subroutine fluxmethod_advection_diffusion_1d(this)
   implicit none
-  class(advdiff1d), intent(inout) :: this
+  class(advection_diffusion_1d), intent(inout) :: this
   ! Local
   integer :: iel
   integer :: ivar
@@ -139,14 +138,14 @@ contains
       enddo
     enddo
 
-  end subroutine fluxmethod_advdiff1d
+  end subroutine fluxmethod_advection_diffusion_1d
 
-  subroutine riemannsolver_advdiff1d(this)
+  subroutine riemannsolver_advection_diffusion_1d(this)
   ! this method uses an linear upwind solver for the
   ! advective flux and the bassi-rebay method for the 
   ! diffusive fluxes
   implicit none
-  class(advdiff1d), intent(inout) :: this
+  class(advection_diffusion_1d), intent(inout) :: this
   ! Local
   integer :: iel
   integer :: ivar
@@ -179,92 +178,7 @@ contains
       enddo
     enddo
 
-  end subroutine riemannsolver_advdiff1d
+  end subroutine riemannsolver_advection_diffusion_1d
 
   
-end module advection_diffusion_model_1d
-
-! ---- Main program below ---- !
-
-program advection_diffusion_1d
-
-  use self_data
-  use advection_diffusion_model_1d
-
-  implicit none
-  character(SELF_INTEGRATOR_LENGTH), parameter :: integrator = 'rk3'
-  integer, parameter :: nvar = 1
-  integer, parameter :: nelem = 50
-  integer,parameter :: controlDegree = 7
-  integer,parameter :: targetDegree = 16
-  real(prec), parameter :: u = 1.0_prec ! velocity
-  real(prec), parameter :: nu = 0.01_prec ! diffusivity
-  real(prec), parameter :: dt = 1.0_prec*10.0_prec**(-4) ! time-step size
-  real(prec), parameter :: endtime = 1.0_prec
-  real(prec), parameter :: iointerval = 0.1_prec
-  integer, parameter :: stepsperio = 1000
-  type(advdiff1d) :: modelobj
-  type(Lagrange),target :: interp
-  type(Mesh1D),target :: mesh
-  type(Geometry1D),target :: geometry
-  type(MPILayer),target :: decomp
-
-  ! Create a mesh using the built-in
-  ! uniform mesh generator.
-  ! The domain is set to x in [0,1]
-  ! We use `nelem` elements
-  call mesh % UniformBlockMesh(nGeo=1,&
-                               nElem=nelem,&
-                               x=(/0.0_prec,1.0_prec/))
-
-  ! We create a domain decomposition.
-  call decomp % Init(enableMPI=.false.)
-  call decomp % GenerateDecomposition(nelem,nelem+1)
-
-  ! Create an interpolant
-  call interp % Init(N=controlDegree, &
-                     controlNodeType=GAUSS, &
-                     M=targetDegree, &
-                     targetNodeType=UNIFORM)
-
-  ! Generate geometry (metric terms) from the mesh elements
-  call geometry % Init(interp,mesh % nElem)
-  call geometry % GenerateFromMesh(mesh)
-
-  ! Initialize the model
-  call modelobj % Init(nvar,mesh,geometry,decomp)
-
-  ! Set the velocity
-  modelobj % u = u
-  !Set the diffusivity
-  modelobj % nu = nu
-
-  ! Set the initial condition
-  call modelobj % solution % SetEquation( 1, 'f = 1.0')
-  call modelobj % solution % SetInteriorFromEquation( geometry, 0.0_prec ) 
-
-  print*, "min, max (interior)", &
-    minval(modelobj % solution % interior % hostdata), &
-    maxval(modelobj % solution % interior % hostdata)
-
-  ! Set the model's time integration method
-  call modelobj % SetTimeIntegrator( integrator )
-
-  ! forward step the model to `endtime` using a time step
-  ! of `dt` and outputing model data every `iointerval`
-  call modelobj % ForwardStep(endtime,dt,iointerval)
-
-  print*, "min, max (interior)", &
-  minval(modelobj % solution % interior % hostdata), &
-  maxval(modelobj % solution % interior % hostdata)
-  
-
-  ! Clean up
-  call modelobj % free()
-  call decomp % free()
-  call mesh % free()
-  call geometry % free()
-  call interp % free()
-  
-
-end program advection_diffusion_1d
+end module self_advection_diffusion_1d
