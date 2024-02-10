62f3b5a0404f36408b0934f6afd0df9f5a2c2c8b
Joe Schoonover <joe@fluidnumerics.com>
2024-02-10 15:05:47 +0000

    Clean up dependency on hipfort

--- util/install_noether.sh
+++ util/install_noether.sh
@@ -6,8 +6,10 @@ BUILD_TYPE=coverage
 SRC_DIR=$(pwd)
 BUILD_DIR=${SRC_DIR}/build
 
-# module load gcc/13.2.0
-module load hip/6.0.2 hipfort/6.0.2 openmpi hdf5 feq-parse
+module load gcc/13.2.0
+module load hip/5.7.3 hipfort/5.7.1 openmpi hdf5 feq-parse
+#module load hip/6.0.2 hipfort/6.0.2 openmpi hdf5 feq-parse
+
 
 # Clean out any old builds
 rm -rf ${BUILD_DIR}
@@ -16,9 +18,7 @@ rm -rf ${WORKSPACE_ROOT}/*
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
 
-FC=gfortran-12 \
-CC=gcc-12 \
-CXX=g++-12 \
+FC=gfortran \
 FFLAGS="-DDOUBLE_PRECISION" \
 cmake -DCMAKE_PREFIX_PATH=/opt/rocm \
       -DCMAKE_HIP_ARCHITECTURES=${GPU_TARGET} \
@@ -36,8 +36,7 @@ export WORKSPACE=${SRC_DIR}
 
 # Initialize coverage
 mkdir -p ${WORKSPACE_ROOT}/tmp/
-lcov --gcov-tool gcov-12 \
-      --no-external \
+lcov --no-external \
       --capture \
       --initial \
       --directory ${SRC_DIR} \
@@ -49,14 +48,12 @@ ctest --test-dir ${BUILD_DIR}
 #ctest --test-dir ${BUILD_DIR}/blas
 
 # Compile coverage information
-lcov --gcov-tool gcov-12 \
-    --no-external \
+lcov --no-external \
     --capture \
     --directory ${SRC_DIR} \
     --exclude '*/test/*' \
     --output-file ${WORKSPACE_ROOT}/tmp/lcov_test.info
 
-lcov --gcov-tool gcov-12 \
-     --add-tracefile ${WORKSPACE_ROOT}/tmp/lcov_base.info \
+lcov --add-tracefile ${WORKSPACE_ROOT}/tmp/lcov_base.info \
      --add-tracefile ${WORKSPACE_ROOT}/tmp/lcov_test.info \
      --output-file ${SRC_DIR}/lcov.info
