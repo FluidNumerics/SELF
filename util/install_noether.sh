5774a4d9a77a1cb8ecd4c8aea4bd49a70c453a80
Joe Schoonover <joe@fluidnumerics.com>
2024-02-10 15:07:38 +0000

    Work with hip/5.7.3
    Currently hip/6.0.2 results in tests that take a while

--- util/install_noether.sh
+++ util/install_noether.sh
@@ -7,7 +7,7 @@ SRC_DIR=$(pwd)
 BUILD_DIR=${SRC_DIR}/build
 
 module load gcc/13.2.0
-module load hip/6.0.2 hipfort/6.0.2 openmpi hdf5 feq-parse
+module load hip/5.7.3 hipfort/5.7.3 openmpi hdf5 feq-parse
 
 # Clean out any old builds
 rm -rf ${BUILD_DIR}
