6d4ab974f7f53523c7755780b7cdf704408d3647
garrett <garrett@fluidnumerics.com>
2024-03-15 09:58:56 +0000

    Cleanup and documentation for bench.sh

--- bench.sh
+++ bench.sh
@@ -6,23 +6,29 @@
 #SBATCH --partition=mi210
 #SBATCH -o stdout
 #SBATCH -e stderr
-#SBATCH --job-name=op_n_32
 
 export BATCHCOUNT=1000
 
-for OP in "op_n" "op_t"
-do
-    export OPERATION=$OP # use lowercase (op_n, op_t)
-    for PREC in 32 64
-    do
+# Use lowercase "op_n" for normal, "op_t" for transposed.
+for OP in "op_n" "op_t"; do
+    export OPERATION=$OP
+    # Use 32 for single/32-bit, 64 for double/64-bit.
+    for PREC in 32 64; do 
         export PRECISION=$PREC
-        for ROWS in $(seq 8 8 256) 
-        do
-            for COLUMNS in $(seq 8 8 256)
-            do
+        # Use the following to set the bounds and stepsize for number of rows/columns.
+        # It is usually expected to make START equal to STEPSIZE.
+        # If rows/columns will not have the same START/STEPSIZE/STOP, this will need
+        # to be changed in their respective for loops.
+        export STEPSIZE=8
+        export START=$STEPSIZE
+        export STOP=256
+        for ROWS in $(seq $START $STEPSIZE $STOP); do
+            for COLUMNS in $(seq $START $STEPSIZE $STOP); do
                 export PROFILE_DIR=blas_results/${OPERATION}_${PRECISION}/${ROWS}_${COLUMNS}_${BATCHCOUNT}
                 export FILENAME=build/blas/gemvstridedbatched_${OPERATION}_${PRECISION}
-                #if [ ! -d "$PROFILE_DIR" ]; then
+                # If file does not already exist or if the number of files in the folder is not the expected value (9).
+                # The second condition is implemented to handle easy scancel/sbatch from the user.
+                # I.e., if a user stops the job early, you end up with a folder that exists, but has incomplete/missing files.
                 if [ ! -d "$PROFILE_DIR" ] || [ "$(ls -l "$PROFILE_DIR" | grep "^-" | wc -l)" -ne 9 ]; then
                     mkdir -p $PROFILE_DIR
 
