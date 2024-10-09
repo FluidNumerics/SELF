e9a2945807a9e57bb8a19e0917e49f67322afeba
Joe <joe@fluidnumerics.com>
2024-10-08 21:02:59 -0400

    Draft structured, tiled mesh generation

---
+++ test/mesh2d_uniformstructured.f90
@@ -0,0 +1,84 @@
+! //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// !
+!
+! Maintainers : support@fluidnumerics.com
+! Official Repository : https://github.com/FluidNumerics/self/
+!
+! Copyright © 2024 Fluid Numerics LLC
+!
+! Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+!
+! 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+!
+! 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in
+!    the documentation and/or other materials provided with the distribution.
+!
+! 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from
+!    this software without specific prior written permission.
+!
+! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+! HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+! LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+! THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+! THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+!
+! //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// !
+
+program test
+
+  implicit none
+  integer :: exit_code
+
+  exit_code = mesh2d_setup()
+  if(exit_code /= 0) then
+    stop exit_code
+  endif
+
+contains
+  integer function mesh2d_setup() result(r)
+
+    use SELF_Constants
+    use SELF_Lagrange
+    use SELF_Mesh_2D
+    use SELF_Geometry_2D
+
+    implicit none
+
+    integer,parameter :: controlDegree = 7
+    integer,parameter :: targetDegree = 16
+#ifdef DOUBLE_PRECISION
+    real(prec),parameter :: tolerance = 10.0_prec**(-7)
+#else
+    real(prec),parameter :: tolerance = 10.0_prec**(-3)
+#endif
+    type(Lagrange),target :: interp
+    type(Mesh2D),target :: mesh
+    type(SEMQuad),target :: geometry
+    character(LEN=255) :: WORKSPACE
+    integer :: bcids(1:4)
+
+    ! Create an interpolant
+    call interp%Init(N=controlDegree, &
+                     controlNodeType=GAUSS, &
+                     M=targetDegree, &
+                     targetNodeType=UNIFORM)
+
+    ! Create a uniform block mesh
+    bcids(1:4) = [SELF_BC_NONORMALFLOW,SELF_BC_PRESCRIBED,SELF_BC_PRESCRIBED,SELF_BC_PRESCRIBED]
+    call mesh%UniformStructuredMesh( 5, 5, 2, 2, 0.1_prec, 0.1_prec, bcids)
+
+    ! Generate geometry (metric terms) from the mesh elements
+    call geometry%Init(interp,mesh%nElem)
+    call geometry%GenerateFromMesh(mesh)
+    call geometry%WriteTecplot('geometry2d.tec')
+    call mesh%Write('mesh2d.h5')
+
+    ! Clean up
+    call geometry%Free()
+    call mesh%Free()
+    call interp%Free()
+
+    r = 0
+
+  endfunction mesh2d_setup
+endprogram test
