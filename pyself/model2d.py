9376aadf37d81cb701e03a4897d1ddade9e75491
Joe Schoonover <joe@fluidnumerics.com>
2024-11-11 21:02:20 +0000

    Allow model2d attributes to be derived from hdf5 file

--- pyself/model2d.py
+++ pyself/model2d.py
@@ -11,7 +11,6 @@ class model:
         self.pvdata = None # Pyvista data
         self.varnames = None
         self.varunits = None
-        self.nvar = 0
         self.geom = geometry.semquad()
 
     def load(self, hdf5File):
@@ -26,26 +25,56 @@ class model:
             
         if 'controlgrid' in list(f.keys()):
 
-            s = f['controlgrid/solution']
-            self.solution = []
-            i = 0
-            for k in s.keys():
-                if k == 'metadata':
-                    for v in s[f"{k}/name"].keys():
-                        self.solution.append( {"name":s[f"{k}/name/{v}"][()][0].decode('utf-8'), "units":s[f"{k}/units/{v}"][()][0].decode('utf-8'), 'data': None} )
-                    self.nvar = len(self.solution)
-                else:
-                    d = s[k]
-                    N = d.shape[2]
-                    # Find index for this field
-                    i=0
-                    for sol in self.solution:
-                        if sol['name'] == k:
-                            break
-                        else:
-                            i+=1
+            controlgrid = f['controlgrid']
+            for group_name in controlgrid.keys():
 
-                    self.solution[i]['data'] = da.from_array(d, chunks=(self.geom.daskChunkSize,N,N))
+                if( group_name == 'geometry' ):
+                    continue
+
+                group = controlgrid[group_name]
+                # Create a list to hold data for this group
+                setattr(self, group_name, [])
+                group_data = getattr(self, group_name)
+                print(f"Loading {group_name} group")
+                
+                # Load metadata information
+                if( 'metadata' in list(group.keys()) ):
+                    for v in group[f"metadata/name"].keys():
+
+                        name = group[f"metadata/name/{v}"].asstr()[()][0]
+                        try:
+                            units = group[f"metadata/units/{v}"].asstr()[()][0]
+                        except:
+                            units = "error"
+
+                        group_data.append({
+                            "name": name,
+                            "units": units,
+                            'data': None
+                        })
+                else:
+                    print(f"Error: /controlgrid/{group_name}/metadata group not found in {hdf5File}.")
+                    return 1
+
+                for k in group.keys():
+                    k_decoded = k.encode('utf-8').decode('utf-8')
+                    if k == 'metadata':
+                        continue
+                    else:
+                        print(f"Loading {k_decoded} field")
+                        # Load the actual data
+                        d = group[k]
+                        N = d.shape[2]
+
+                        # Find index for this field
+                        i = 0
+                        for sol in group_data:
+                            if sol['name'] == k_decoded:
+                                break
+                            else:
+                                i += 1
+
+                        group_data[i]['data'] = da.from_array(d, chunks=(self.geom.daskChunkSize, N, N))
 
             self.generate_pyvista()
 
@@ -97,8 +126,13 @@ class model:
 
         # Load fields into pvdata
         k = 0
-        for var in self.solution:
-            self.pvdata.point_data.set_array(var['data'].flatten(),var['name'])
-            k+=1
+        for attr in self.__dict__:
+            if not attr in ['pvdata','varnames','varunits','geom']:
+                controlgroup = getattr(self, attr)
+                #print(f"Loading {attr} into pvdata")
+                for var in controlgroup:
+                 #   print(f"Loading {var['name']} into pvdata")
+                    self.pvdata.point_data.set_array(var['data'].flatten(),var['name'])
+                    k+=1
             
         print(self.pvdata)
