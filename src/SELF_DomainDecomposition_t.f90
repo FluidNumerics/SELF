c0fec08cf666d8c88c2443537788cb7f8e7bcac2
Joe <joe@fluidnumerics.com>
2024-11-18 11:08:37 -0500

    Formatting

--- src/SELF_DomainDecomposition_t.f90
+++ src/SELF_DomainDecomposition_t.f90
@@ -26,13 +26,13 @@
 
 module SELF_DomainDecomposition_t
 
-   use SELF_Constants
-   use SELF_Lagrange
-   use SELF_SupportRoutines
-   use mpi
-   use iso_c_binding
+  use SELF_Constants
+  use SELF_Lagrange
+  use SELF_SupportRoutines
+  use mpi
+  use iso_c_binding
 
-   implicit none
+  implicit none
 
   type DomainDecomposition_t
     logical :: mpiEnabled = .false.
@@ -49,200 +49,200 @@ module SELF_DomainDecomposition_t
     integer,allocatable :: requests(:)
     integer,allocatable :: stats(:,:)
 
-   contains
+  contains
 
-      procedure :: Init => Init_DomainDecomposition_t
-      procedure :: Free => Free_DomainDecomposition_t
+    procedure :: Init => Init_DomainDecomposition_t
+    procedure :: Free => Free_DomainDecomposition_t
 
-      procedure :: GenerateDecomposition => GenerateDecomposition_DomainDecomposition_t
-      procedure :: SetElemToRank => SetElemToRank_DomainDecomposition_t
+    procedure :: GenerateDecomposition => GenerateDecomposition_DomainDecomposition_t
+    procedure :: SetElemToRank => SetElemToRank_DomainDecomposition_t
 
-      procedure, public :: FinalizeMPIExchangeAsync
+    procedure,public :: FinalizeMPIExchangeAsync
 
-   end type DomainDecomposition_t
+  endtype DomainDecomposition_t
 
 contains
 
-   subroutine Init_DomainDecomposition_t(this, enableMPI)
+  subroutine Init_DomainDecomposition_t(this,enableMPI)
 #undef __FUNC__
 #define __FUNC__ "Init_DomainDecomposition_t"
-      implicit none
-      class(DomainDecomposition_t), intent(inout) :: this
-      logical, intent(in) :: enableMPI
-      ! Local
-      integer       :: ierror
+    implicit none
+    class(DomainDecomposition_t),intent(inout) :: this
+    logical,intent(in) :: enableMPI
+    ! Local
+    integer       :: ierror
 
-      this%mpiComm = 0
-      this%mpiPrec = prec
-      this%rankId = 0
-      this%nRanks = 1
-      this%nElem = 0
-      this%mpiEnabled = enableMPI
+    this%mpiComm = 0
+    this%mpiPrec = prec
+    this%rankId = 0
+    this%nRanks = 1
+    this%nElem = 0
+    this%mpiEnabled = enableMPI
 
-      if (enableMPI) then
-         this%mpiComm = MPI_COMM_WORLD
-         print *, __FILE__, " : Initializing MPI"
-         call mpi_init(ierror)
-         call mpi_comm_rank(this%mpiComm, this%rankId, ierror)
-         call mpi_comm_size(this%mpiComm, this%nRanks, ierror)
-         print *, __FILE__, " : Rank ", this%rankId + 1, "/", this%nRanks, " checking in."
-      else
-         print *, __FILE__, " : MPI not initialized. No domain decomposition used."
-      end if
+    if(enableMPI) then
+      this%mpiComm = MPI_COMM_WORLD
+      print*,__FILE__," : Initializing MPI"
+      call mpi_init(ierror)
+      call mpi_comm_rank(this%mpiComm,this%rankId,ierror)
+      call mpi_comm_size(this%mpiComm,this%nRanks,ierror)
+      print*,__FILE__," : Rank ",this%rankId+1,"/",this%nRanks," checking in."
+    else
+      print*,__FILE__," : MPI not initialized. No domain decomposition used."
+    endif
 
-      if (prec == real32) then
-         this%mpiPrec = MPI_FLOAT
-      else
-         this%mpiPrec = MPI_DOUBLE
-      end if
+    if(prec == real32) then
+      this%mpiPrec = MPI_FLOAT
+    else
+      this%mpiPrec = MPI_DOUBLE
+    endif
 
-      allocate (this%offsetElem(1:this%nRanks + 1))
+    allocate(this%offsetElem(1:this%nRanks+1))
 
-      this%initialized = .true.
+    this%initialized = .true.
 
     this%initialized = .true.
 
   endsubroutine Init_DomainDecomposition_t
 
-   subroutine Free_DomainDecomposition_t(this)
-      implicit none
-      class(DomainDecomposition_t), intent(inout) :: this
-      ! Local
-      integer :: ierror
+  subroutine Free_DomainDecomposition_t(this)
+    implicit none
+    class(DomainDecomposition_t),intent(inout) :: this
+    ! Local
+    integer :: ierror
 
-      if (associated(this%offSetElem)) then
-         deallocate (this%offSetElem)
-      end if
-      if (associated(this%elemToRank)) then
-         deallocate (this%elemToRank)
-      end if
+    if(associated(this%offSetElem)) then
+      deallocate(this%offSetElem)
+    endif
+    if(associated(this%elemToRank)) then
+      deallocate(this%elemToRank)
+    endif
 
-      if (allocated(this%requests)) deallocate (this%requests)
-      if (allocated(this%stats)) deallocate (this%stats)
+    if(allocated(this%requests)) deallocate(this%requests)
+    if(allocated(this%stats)) deallocate(this%stats)
 
-      if (this%mpiEnabled) then
-         print *, __FILE__, " : Rank ", this%rankId + 1, "/", this%nRanks, " checking out."
-         call MPI_FINALIZE(ierror)
-      end if
+    if(this%mpiEnabled) then
+      print*,__FILE__," : Rank ",this%rankId+1,"/",this%nRanks," checking out."
+      call MPI_FINALIZE(ierror)
+    endif
 
-   end subroutine Free_DomainDecomposition_t
+  endsubroutine Free_DomainDecomposition_t
 
-   subroutine GenerateDecomposition_DomainDecomposition_t(this, nGlobalElem, maxMsg)
-      implicit none
-      class(DomainDecomposition_t), intent(inout) :: this
-      integer, intent(in) :: nGlobalElem
-      integer, intent(in) :: maxMsg
+  subroutine GenerateDecomposition_DomainDecomposition_t(this,nGlobalElem,maxMsg)
+    implicit none
+    class(DomainDecomposition_t),intent(inout) :: this
+    integer,intent(in) :: nGlobalElem
+    integer,intent(in) :: maxMsg
 
-      call this%setElemToRank(nGlobalElem)
-      if (allocated(this%requests)) deallocate (this%requests)
-      if (allocated(this%stats)) deallocate (this%stats)
+    call this%setElemToRank(nGlobalElem)
+    if(allocated(this%requests)) deallocate(this%requests)
+    if(allocated(this%stats)) deallocate(this%stats)
 
-      allocate (this%requests(1:maxMsg))
-      allocate (this%stats(MPI_STATUS_SIZE, 1:maxMsg))
-      this%maxMsg = maxMsg
+    allocate(this%requests(1:maxMsg))
+    allocate(this%stats(MPI_STATUS_SIZE,1:maxMsg))
+    this%maxMsg = maxMsg
 
-      print *, __FILE__//" : Rank ", this%rankId + 1, " : n_elements = ", &
-         this%offSetElem(this%rankId + 2) - this%offSetElem(this%rankId + 1)
+    print*,__FILE__//" : Rank ",this%rankId+1," : n_elements = ", &
+      this%offSetElem(this%rankId+2)-this%offSetElem(this%rankId+1)
 
-   end subroutine GenerateDecomposition_DomainDecomposition_t
+  endsubroutine GenerateDecomposition_DomainDecomposition_t
 
-   subroutine SetElemToRank_DomainDecomposition_t(this, nElem)
-      implicit none
-      class(DomainDecomposition_t), intent(inout) :: this
-      integer, intent(in) :: nElem
-      ! Local
-      integer :: iel
+  subroutine SetElemToRank_DomainDecomposition_t(this,nElem)
+    implicit none
+    class(DomainDecomposition_t),intent(inout) :: this
+    integer,intent(in) :: nElem
+    ! Local
+    integer :: iel
 
-      this%nElem = nElem
+    this%nElem = nElem
 
-      allocate (this%elemToRank(1:nelem))
+    allocate(this%elemToRank(1:nelem))
 
-      call DomainDecomp(nElem, &
-                        this%nRanks, &
-                        this%offSetElem)
+    call DomainDecomp(nElem, &
+                      this%nRanks, &
+                      this%offSetElem)
 
-      do iel = 1, nElem
-         call ElemToRank(this%nRanks, &
-                         this%offSetElem, &
-                         iel, &
-                         this%elemToRank(iel))
-      end do
+    do iel = 1,nElem
+      call ElemToRank(this%nRanks, &
+                      this%offSetElem, &
+                      iel, &
+                      this%elemToRank(iel))
+    enddo
 
-   end subroutine SetElemToRank_DomainDecomposition_t
+  endsubroutine SetElemToRank_DomainDecomposition_t
 
-   subroutine DomainDecomp(nElems, nDomains, offSetElem)
-      ! From https://www.hopr-project.org/externals/Meshformat.pdf, Algorithm 4
-      implicit none
-      integer, intent(in) :: nElems
-      integer, intent(in) :: nDomains
-      integer, intent(out) :: offsetElem(0:nDomains)
-      ! Local
-      integer :: nLocalElems
-      integer :: remainElems
-      integer :: iDom
+  subroutine DomainDecomp(nElems,nDomains,offSetElem)
+    ! From https://www.hopr-project.org/externals/Meshformat.pdf, Algorithm 4
+    implicit none
+    integer,intent(in) :: nElems
+    integer,intent(in) :: nDomains
+    integer,intent(out) :: offsetElem(0:nDomains)
+    ! Local
+    integer :: nLocalElems
+    integer :: remainElems
+    integer :: iDom
 
-      nLocalElems = nElems/nDomains
-      remainElems = nElems - nLocalElems*nDomains
-      do iDom = 0, nDomains - 1
-         offSetElem(iDom) = iDom*nLocalElems + min(iDom, remainElems)
-      end do
-      offSetElem(nDomains) = nElems
+    nLocalElems = nElems/nDomains
+    remainElems = nElems-nLocalElems*nDomains
+    do iDom = 0,nDomains-1
+      offSetElem(iDom) = iDom*nLocalElems+min(iDom,remainElems)
+    enddo
+    offSetElem(nDomains) = nElems
 
-   end subroutine DomainDecomp
+  endsubroutine DomainDecomp
 
-   subroutine ElemToRank(nDomains, offsetElem, elemID, domain)
-      ! From https://www.hopr-project.org/externals/Meshformat.pdf, Algorithm 7
-      !   "Find domain containing element index"
-      !
-      implicit none
-      integer, intent(in) :: nDomains
-      integer, intent(in) :: offsetElem(0:nDomains)
-      integer, intent(in) :: elemID
-      integer, intent(out) :: domain
-      ! Local
-      integer :: maxSteps
-      integer :: low, up, mid
-      integer :: i
+  subroutine ElemToRank(nDomains,offsetElem,elemID,domain)
+    ! From https://www.hopr-project.org/externals/Meshformat.pdf, Algorithm 7
+    !   "Find domain containing element index"
+    !
+    implicit none
+    integer,intent(in) :: nDomains
+    integer,intent(in) :: offsetElem(0:nDomains)
+    integer,intent(in) :: elemID
+    integer,intent(out) :: domain
+    ! Local
+    integer :: maxSteps
+    integer :: low,up,mid
+    integer :: i
 
-      domain = 0
-      maxSteps = int(log10(real(nDomains))/log10(2.0)) + 1
-      low = 0
-      up = nDomains - 1
+    domain = 0
+    maxSteps = int(log10(real(nDomains))/log10(2.0))+1
+    low = 0
+    up = nDomains-1
 
-      if (offsetElem(low) < elemID .and. elemID <= offsetElem(low + 1)) then
-         domain = low
-      elseif (offsetElem(up) < elemID .and. elemID <= offsetElem(up + 1)) then
-         domain = up
-      else
-         do i = 1, maxSteps
-            mid = (up - low)/2 + low
-            if (offsetElem(mid) < elemID .and. elemID <= offsetElem(mid + 1)) then
-               domain = mid
-               return
-            elseif (elemID > offsetElem(mid + 1)) then
-               low = mid + 1
-            else
-               up = mid
-            end if
-         end do
-      end if
+    if(offsetElem(low) < elemID .and. elemID <= offsetElem(low+1)) then
+      domain = low
+    elseif(offsetElem(up) < elemID .and. elemID <= offsetElem(up+1)) then
+      domain = up
+    else
+      do i = 1,maxSteps
+        mid = (up-low)/2+low
+        if(offsetElem(mid) < elemID .and. elemID <= offsetElem(mid+1)) then
+          domain = mid
+          return
+        elseif(elemID > offsetElem(mid+1)) then
+          low = mid+1
+        else
+          up = mid
+        endif
+      enddo
+    endif
 
-   end subroutine ElemToRank
+  endsubroutine ElemToRank
 
-   subroutine FinalizeMPIExchangeAsync(mpiHandler)
-      class(DomainDecomposition_t), intent(inout) :: mpiHandler
-      ! Local
-      integer :: ierror
-      integer :: msgCount
+  subroutine FinalizeMPIExchangeAsync(mpiHandler)
+    class(DomainDecomposition_t),intent(inout) :: mpiHandler
+    ! Local
+    integer :: ierror
+    integer :: msgCount
 
-      if (mpiHandler%mpiEnabled) then
-         msgCount = mpiHandler%msgCount
-         call MPI_WaitAll(msgCount, &
-                          mpiHandler%requests(1:msgCount), &
-                          mpiHandler%stats(1:MPI_STATUS_SIZE, 1:msgCount), &
-                          iError)
-      end if
+    if(mpiHandler%mpiEnabled) then
+      msgCount = mpiHandler%msgCount
+      call MPI_WaitAll(msgCount, &
+                       mpiHandler%requests(1:msgCount), &
+                       mpiHandler%stats(1:MPI_STATUS_SIZE,1:msgCount), &
+                       iError)
+    endif
 
-   end subroutine FinalizeMPIExchangeAsync
+  endsubroutine FinalizeMPIExchangeAsync
 
-end module SELF_DomainDecomposition_t
+endmodule SELF_DomainDecomposition_t
--- test/mappedvectordgdivergence_3d_linear_sideexchange_mpi.f90
+++ test/mappedvectordgdivergence_3d_linear_sideexchange_mpi.f90
@@ -26,33 +26,33 @@
 
 program test
 
-   implicit none
-   integer :: exit_code
+  implicit none
+  integer :: exit_code
 
-   exit_code = mappedvectordgdivergence_3d_linear()
-   if (exit_code /= 0) then
-      stop exit_code
-   end if
+  exit_code = mappedvectordgdivergence_3d_linear()
+  if(exit_code /= 0) then
+    stop exit_code
+  endif
 
 contains
-   integer function mappedvectordgdivergence_3d_linear() result(r)
+  integer function mappedvectordgdivergence_3d_linear() result(r)
 
-      use SELF_Constants
-      use SELF_Lagrange
-      use SELF_Mesh_3D
-      use SELF_Geometry_3D
-      use SELF_MappedScalar_3D
-      use SELF_MappedVector_3D
+    use SELF_Constants
+    use SELF_Lagrange
+    use SELF_Mesh_3D
+    use SELF_Geometry_3D
+    use SELF_MappedScalar_3D
+    use SELF_MappedVector_3D
 
-      implicit none
+    implicit none
 
-      integer, parameter :: controlDegree = 7
-      integer, parameter :: targetDegree = 16
-      integer, parameter :: nvar = 1
+    integer,parameter :: controlDegree = 7
+    integer,parameter :: targetDegree = 16
+    integer,parameter :: nvar = 1
 #ifdef DOUBLE_PRECISION
-      real(prec), parameter :: tolerance = 10.0_prec**(-7)
+    real(prec),parameter :: tolerance = 10.0_prec**(-7)
 #else
-      real(prec), parameter :: tolerance = 10.0_prec**(-3)
+    real(prec),parameter :: tolerance = 10.0_prec**(-3)
 #endif
     type(Lagrange),target :: interp
     type(Mesh3D),target :: mesh
@@ -67,95 +67,95 @@ contains
     call get_environment_variable("WORKSPACE",WORKSPACE)
     call mesh%Read_HOPr(trim(WORKSPACE)//"/share/mesh/Block3D/Block3D_mesh.h5",enableDomainDecomposition=.true.)
 
-      ! Create an interpolant
-      call interp%Init(N=controlDegree, &
-                       controlNodeType=GAUSS, &
-                       M=targetDegree, &
-                       targetNodeType=UNIFORM)
+    ! Create an interpolant
+    call interp%Init(N=controlDegree, &
+                     controlNodeType=GAUSS, &
+                     M=targetDegree, &
+                     targetNodeType=UNIFORM)
 
     ! Generate geometry (metric terms) from the mesh elements
     call geometry%Init(interp,mesh%nElem)
     call geometry%GenerateFromMesh(mesh)
 
-      call f%Init(interp, nvar, mesh%nelem)
-      call df%Init(interp, nvar, mesh%nelem)
-      call f%AssociateGeometry(geometry)
+    call f%Init(interp,nvar,mesh%nelem)
+    call df%Init(interp,nvar,mesh%nelem)
+    call f%AssociateGeometry(geometry)
 
-      call f%SetEquation(1, 1, 'f = x') ! x-component
-      call f%SetEquation(2, 1, 'f = y') ! y-component
-      call f%SetEquation(3, 1, 'f = 0') ! z-component
+    call f%SetEquation(1,1,'f = x') ! x-component
+    call f%SetEquation(2,1,'f = y') ! y-component
+    call f%SetEquation(3,1,'f = 0') ! z-component
 
-      call f%SetInteriorFromEquation(geometry, 0.0_prec)
-      print *, "min, max (interior)", minval(f%interior), maxval(f%interior)
-      call f%boundaryInterp()
+    call f%SetInteriorFromEquation(geometry,0.0_prec)
+    print*,"min, max (interior)",minval(f%interior),maxval(f%interior)
+    call f%boundaryInterp()
 
-      print *, "Exchanging data on element faces"
+    print*,"Exchanging data on element faces"
 
-      call f%SideExchange(mesh)
-      call f%UpdateHost()
+    call f%SideExchange(mesh)
+    call f%UpdateHost()
 
-      print *, "Setting boundary conditions"
-      ! Set boundary conditions
-      do iEl = 1, f%nElem
-         do k = 1, 6
-            e2 = mesh%sideInfo(3, k, iel) ! Neighbor Element (global id)
+    print*,"Setting boundary conditions"
+    ! Set boundary conditions
+    do iEl = 1,f%nElem
+      do k = 1,6
+        e2 = mesh%sideInfo(3,k,iel) ! Neighbor Element (global id)
 
-            if (e2 == 0) then ! Exterior edge
-               do j = 1, f%interp%N + 1
-                  do i = 1, f%interp%N + 1
-                     f%extboundary(i, j, k, iEl, 1, 1) = f%boundary(i, j, k, iEl, 1, 1)
-                     f%extboundary(i, j, k, iEl, 1, 2) = f%boundary(i, j, k, iEl, 1, 2)
-                     f%extboundary(i, j, k, iEl, 1, 3) = f%boundary(i, j, k, iEl, 1, 3)
-                  end do
-               end do
-            end if
-         end do
-      end do
+        if(e2 == 0) then ! Exterior edge
+          do j = 1,f%interp%N+1
+            do i = 1,f%interp%N+1
+              f%extboundary(i,j,k,iEl,1,1) = f%boundary(i,j,k,iEl,1,1)
+              f%extboundary(i,j,k,iEl,1,2) = f%boundary(i,j,k,iEl,1,2)
+              f%extboundary(i,j,k,iEl,1,3) = f%boundary(i,j,k,iEl,1,3)
+            enddo
+          enddo
+        endif
+      enddo
+    enddo
 
-      print *, "Calculating boundary normal flux"
-      do iEl = 1, f%nElem
-         do k = 1, 6
-            do j = 1, f%interp%N + 1
-               do i = 1, f%interp%N + 1
+    print*,"Calculating boundary normal flux"
+    do iEl = 1,f%nElem
+      do k = 1,6
+        do j = 1,f%interp%N+1
+          do i = 1,f%interp%N+1
 
-                  ! Get the boundary normals on cell edges from the mesh geometry
-                  nhat(1:3) = geometry%nHat%boundary(i, j, k, iEl, 1, 1:3)
-                  nmag = geometry%nScale%boundary(i, j, k, iEl, 1)
-                  fx = 0.5*(f%boundary(i, j, k, iEl, 1, 1) + f%extboundary(i, j, k, iEl, 1, 1))
-                  fy = 0.5*(f%boundary(i, j, k, iEl, 1, 2) + f%extboundary(i, j, k, iEl, 1, 2))
-                  fz = 0.5*(f%boundary(i, j, k, iEl, 1, 3) + f%extboundary(i, j, k, iEl, 1, 3))
+            ! Get the boundary normals on cell edges from the mesh geometry
+            nhat(1:3) = geometry%nHat%boundary(i,j,k,iEl,1,1:3)
+            nmag = geometry%nScale%boundary(i,j,k,iEl,1)
+            fx = 0.5*(f%boundary(i,j,k,iEl,1,1)+f%extboundary(i,j,k,iEl,1,1))
+            fy = 0.5*(f%boundary(i,j,k,iEl,1,2)+f%extboundary(i,j,k,iEl,1,2))
+            fz = 0.5*(f%boundary(i,j,k,iEl,1,3)+f%extboundary(i,j,k,iEl,1,3))
 
-                  f%boundaryNormal(i, j, k, iEl, 1) = (fx*nhat(1) + fy*nhat(2) + fz*nhat(3))*nmag
-               end do
-            end do
-         end do
-      end do
-      call f%UpdateDevice()
+            f%boundaryNormal(i,j,k,iEl,1) = (fx*nhat(1)+fy*nhat(2)+fz*nhat(3))*nmag
+          enddo
+        enddo
+      enddo
+    enddo
+    call f%UpdateDevice()
 
 #ifdef ENABLE_GPU
-      call f%MappedDGDivergence(df%interior_gpu)
+    call f%MappedDGDivergence(df%interior_gpu)
 #else
-      call f%MappedDGDivergence(df%interior)
+    call f%MappedDGDivergence(df%interior)
 #endif
-      call df%UpdateHost()
+    call df%UpdateHost()
 
-      ! Calculate diff from exact
-      df%interior = abs(df%interior - 2.0_prec)
+    ! Calculate diff from exact
+    df%interior = abs(df%interior-2.0_prec)
 
-      if (maxval(df%interior) <= tolerance) then
-         r = 0
-      else
-         print *, "max error (tolerance)", maxval(df%interior), tolerance
-         r = 1
-      end if
+    if(maxval(df%interior) <= tolerance) then
+      r = 0
+    else
+      print*,"max error (tolerance)",maxval(df%interior),tolerance
+      r = 1
+    endif
 
-      ! Clean up
-      call f%DissociateGeometry()
-      call geometry%Free()
-      call mesh%Free()
-      call interp%Free()
-      call f%free()
-      call df%free()
+    ! Clean up
+    call f%DissociateGeometry()
+    call geometry%Free()
+    call mesh%Free()
+    call interp%Free()
+    call f%free()
+    call df%free()
 
-   end function mappedvectordgdivergence_3d_linear
-end program test
+  endfunction mappedvectordgdivergence_3d_linear
+endprogram test
