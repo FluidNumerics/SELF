ab374de85872e69644be6118fc7e04ad8dbdff17
Joe Schoonover <joe@fluidnumerics.com>
2025-03-12 15:59:14 -0400

    Allow lib to be set dyamically in the SelfModel init

--- pyself/interface.py
+++ pyself/interface.py
@@ -6,16 +6,27 @@ from pyself.config import SelfModelConfig
 
 ## Add ctypes interface to fortran library
 from ctypes import CDLL, c_int, c_double, c_char_p, POINTER, c_void_p
+from ctypes.util import find_library
 import os
-
-# Load the fortran library
-lib = CDLL(os.path.join(os.path.dirname(__file__), "libself_interface.so"))
 
 
 class SelfModel:
-    def __init__(self, config: SelfModelConfig):
+    def __init__(self, config: SelfModelConfig, lib: str = None):
         self.case_directory = case_directory
         self.config = config
+
+        if lib is None:
+            try:
+                self.lib = CDLL(find_lib("self_interface"))
+            except:
+                raise Exception(
+                    "Could not find the libself_interface.so library. Ensure your LD_LIBRARY_PATH includes the path for libself_interface.so"
+                )
+        else:
+            # To do - check if path exists..
+            # To do - must be libself_interface.so
+            self.lib = CDLL(lib)
+            # To do - exceptions on  CDLL failures.
 
         # To do : Call fortran library to ininitialize the model
         # To do : Add mpi support - pass communicator and rank to fortran library
@@ -39,4 +50,4 @@ class SelfModel:
     # def finalize(self):
 
     # def run(self):
-    #     print("Running SELF model...")
+    #     print("Running SELF model..."
